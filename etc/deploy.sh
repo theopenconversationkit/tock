fc03e08bbbe264c688bbca230eb2da687a20d8ea
vsct-jburet <jburet@voyages-sncf.com>
2017-03-23 18:02:22 +0100

    fix typo
    Signed-off-by: vsct-jburet <jburet@voyages-sncf.com>

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && ["$TRAVIS_TAG" == '']; then
+if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && ["$TRAVIS_TAG" == '']; then
 openssl aes-256-cbc -K $encrypted_30d9879477f5_key -iv $encrypted_30d9879477f5_iv -in etc/codesigning.asc.enc -out etc/codesigning.asc -d
 mvn clean package dokka:javadocJar deploy -Ddeploy --settings etc/deploy-settings.xml
 fi
