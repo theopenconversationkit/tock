7c1b1232900677304395b2f58677eb20f2d234d2
vsct-jburet <jburet@voyages-sncf.com>
2017-03-23 18:54:14 +0100

    fix typo
    Signed-off-by: vsct-jburet <jburet@voyages-sncf.com>

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -4,6 +4,6 @@ echo "pull_request : $TRAVIS_PULL_REQUEST"
 echo "tag : $TRAVIS_TAG"
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ] && [ "$TRAVIS_TAG" = '' ]; then
 openssl aes-256-cbc -K $encrypted_30d9879477f5_key -iv $encrypted_30d9879477f5_iv -in etc/codesigning.asc.enc -out etc/codesigning.asc -d
-gpg --fast-import etc/signingkey.asc
+gpg --fast-import etc/codesigning.asc
 mvn clean package dokka:javadocJar deploy -Ddeploy --settings etc/deploy-settings.xml
 fi
