6642a36c46ab1a82df21dd1f6868eb84dd46b339
Julien Buret <jburet@voyages-sncf.com>
2017-04-08 13:02:51 +0200

    introduce ContextValue
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

---
+++ bot/engine/src/main/kotlin/fr/vsct/tock/bot/engine/dialog/ContextValue.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2017 VSCT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.vsct.tock.bot.engine.dialog
+
+import fr.vsct.tock.nlp.entity.Value
+import ft.vsct.tock.nlp.api.client.model.Entity
+import ft.vsct.tock.nlp.api.client.model.EntityValue
+
+/**
+ *
+ */
+data class ContextValue(
+        val start: Int,
+        val end: Int,
+        val entity: Entity,
+        val content: String,
+        var value: Value? = null,
+        var evaluated: Boolean = false
+) {
+
+    constructor(sentence: String, value: EntityValue)
+            : this(
+            value.start,
+            value.end,
+            value.entity,
+            sentence.substring(value.start, value.end),
+            value.value,
+            value.evaluated
+    )
+
+    override fun toString(): String {
+        return if (evaluated) value?.toString() ?: "null" else content
+    }
+
+    fun changeValue(newValue: Value?): ContextValue {
+        value = newValue
+        evaluated = true
+        return this
+    }
+
+
+}
