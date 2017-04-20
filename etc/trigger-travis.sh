a7dba9b632d0ab3aed28656b0f66166f3512bb1a
Julien Buret <jburet@voyages-sncf.com>
2017-04-20 21:44:01 +0200

    trigger dependant projects builds
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

---
+++ etc/trigger-travis.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+body='{
+  "request": {
+    "branch":"master"
+  }
+}'
+
+function travis-api {
+ echo "launch build for $1/$2"
+ curl -s -X POST \
+   -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token $TRAVIS_ACCESS_TOKEN" \
+  -d "$body" \
+  "https://api.travis-ci.org/repo/$1%2F$2/requests"
+}
+
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ] && [ "$TRAVIS_TAG" = '' ]; then
+  travis-api "voyages-sncf-technologies" "tock-corenlp"
+  travis-api "voyages-sncf-technologies" "tock-bot-open-data"
+fi
