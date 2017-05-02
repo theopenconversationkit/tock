226a8ba6b0f540836492dd863d1b5b9fc0275fd2
Julien Buret <jburet@voyages-sncf.com>
2017-05-02 11:27:57 +0200

    add docker hub in documentation [fr]
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

--- docs/fr/mkdocs/js/text.js
+++ docs/fr/mkdocs/js/text.js
@@ -1,17 +1,7 @@
-/*
- * Copyright (C) 2017 VSCT
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/**
+ * @license RequireJS text 2.0.12 Copyright (c) 2010-2014, The Dojo Foundation All Rights Reserved.
+ * Available via the MIT or new BSD license.
+ * see: http://github.com/requirejs/text for details
  */
 /*jslint regexp: true */
 /*global require, XMLHttpRequest, ActiveXObject,
