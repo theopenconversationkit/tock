5f955345e6e1dd805c918912b75b7a89e784dee2
Julien Buret <jburet@voyages-sncf.com>
2017-05-15 14:14:46 +0200

    do not trigger docker build for each commit - there is a cron for tock-docker
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

--- etc/trigger-travis.sh
+++ etc/trigger-travis.sh
@@ -20,5 +20,4 @@ function travis-api {
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ] && [ "$TRAVIS_TAG" = '' ]; then
   travis-api "voyages-sncf-technologies" "tock-corenlp"
   travis-api "voyages-sncf-technologies" "tock-bot-open-data"
-  travis-api "voyages-sncf-technologies" "tock-docker"
 fi
