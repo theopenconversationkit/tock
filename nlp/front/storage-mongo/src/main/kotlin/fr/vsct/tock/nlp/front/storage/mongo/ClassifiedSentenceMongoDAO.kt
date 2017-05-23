705421cee581da7b2f89463b03fd1bf63f09f3e8
Julien Buret <jburet@voyages-sncf.com>
2017-05-23 21:16:49 +0200

    resolves #23 Allow analysing text greater than 1024 chars
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

--- nlp/front/storage-mongo/src/main/kotlin/fr/vsct/tock/nlp/front/storage/mongo/ClassifiedSentenceMongoDAO.kt
+++ nlp/front/storage-mongo/src/main/kotlin/fr/vsct/tock/nlp/front/storage/mongo/ClassifiedSentenceMongoDAO.kt
@@ -21,6 +21,7 @@ import com.mongodb.client.model.IndexOptions
 import com.mongodb.client.model.Sorts
 import com.mongodb.client.model.UpdateOptions
 import fr.vsct.tock.nlp.front.service.storage.ClassifiedSentenceDAO
+import fr.vsct.tock.nlp.front.shared.config.Classification
 import fr.vsct.tock.nlp.front.shared.config.ClassifiedSentence
 import fr.vsct.tock.nlp.front.shared.config.ClassifiedSentenceStatus
 import fr.vsct.tock.nlp.front.shared.config.ClassifiedSentenceStatus.inbox
@@ -39,6 +40,7 @@ import org.litote.kmongo.getCollection
 import org.litote.kmongo.json
 import org.litote.kmongo.replaceOne
 import org.litote.kmongo.updateMany
+import java.time.Instant
 import java.util.Locale
 
 /**
@@ -46,8 +48,37 @@ import java.util.Locale
  */
 object ClassifiedSentenceMongoDAO : ClassifiedSentenceDAO {
 
-    private val col: MongoCollection<ClassifiedSentence> by lazy {
-        val c = MongoFrontConfiguration.database.getCollection<ClassifiedSentence>()
+    //wrapper to workaround the 1024 chars limit for String indexes
+    private fun textKey(text: String): String
+            = if (text.length > 512) text.substring(0, Math.min(512, text.length)) else text
+
+    private data class ClassifiedSentenceCol(val text: String,
+                                             val fullText: String = text,
+                                             val language: Locale,
+                                             val applicationId: String,
+                                             val creationDate: Instant,
+                                             val updateDate: Instant,
+                                             val status: ClassifiedSentenceStatus,
+                                             val classification: Classification) {
+
+        constructor(sentence: ClassifiedSentence) :
+                this(
+                        textKey(sentence.text),
+                        sentence.text,
+                        sentence.language,
+                        sentence.applicationId,
+                        sentence.creationDate,
+                        sentence.updateDate,
+                        sentence.status,
+                        sentence.classification
+                )
+
+        fun toSentence(): ClassifiedSentence
+                = ClassifiedSentence(fullText, language, applicationId, creationDate, updateDate, status, classification)
+    }
+
+    private val col: MongoCollection<ClassifiedSentenceCol> by lazy {
+        val c = MongoFrontConfiguration.database.getCollection<ClassifiedSentenceCol>("classified_sentence")
         c.createIndex("{'text':1,'language':1,'applicationId':1}", IndexOptions().unique(true))
         c.createIndex("{'language':1,'applicationId':1,'status':1}")
         c.createIndex("{'status':1}")
@@ -65,7 +96,7 @@ object ClassifiedSentenceMongoDAO : ClassifiedSentenceDAO {
                 if (status != null) "'status':${status.json}" else null
         ).joinToString(prefix = "{", postfix = "}")
 
-        return col.find(query).toList()
+        return col.find(query).toList().map { it.toSentence() }
     }
 
     override fun switchSentencesStatus(sentences: List<ClassifiedSentence>, newStatus: ClassifiedSentenceStatus) {
@@ -83,7 +114,7 @@ object ClassifiedSentenceMongoDAO : ClassifiedSentenceDAO {
     }
 
     override fun save(sentence: ClassifiedSentence) {
-        col.replaceOne("{'text':${sentence.text.json},'language':${sentence.language.json},'applicationId':${sentence.applicationId.json}}", sentence, UpdateOptions().upsert(true))
+        col.replaceOne("{'text':${textKey(sentence.text).json},'language':${sentence.language.json},'applicationId':${sentence.applicationId.json}}", ClassifiedSentenceCol(sentence), UpdateOptions().upsert(true))
     }
 
     override fun search(query: SentencesQuery): SentencesQueryResult {
@@ -97,7 +128,7 @@ object ClassifiedSentenceMongoDAO : ClassifiedSentenceDAO {
                     listOfNotNull(
                             "'applicationId':${applicationId.json}",
                             "'language':${language.json}",
-                            if (search.isNullOrBlank()) null else if (query.onlyExactMatch) "'text':${search!!.json}" else "'text':/${search!!.trim()}/i",
+                            if (search.isNullOrBlank()) null else if (query.onlyExactMatch) "'fullText':${search!!.json}" else "'fullText':/${search!!.trim()}/i",
                             if (intentId.isNullOrBlank()) null else "'classification.intentId':${intentId!!.json}",
                             if (filterStatus.isEmpty()) null else filterStatus
                     ).joinToString(",", "{", "}")
@@ -105,7 +136,7 @@ object ClassifiedSentenceMongoDAO : ClassifiedSentenceDAO {
             if (count > start) {
                 val list = col.find(filter)
                         .skip(start.toInt()).limit(size).sort(Sorts.descending("_id")).toList()
-                return SentencesQueryResult(count, list)
+                return SentencesQueryResult(count, list.map { it.toSentence() })
             } else {
                 return SentencesQueryResult(0, emptyList())
             }
