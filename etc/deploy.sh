693bee15f562e0e95e2e05ba6db96e6e4bdf0263
Julien Buret <jburet@voyages-sncf.com>
2017-06-26 00:29:07 +0200

    skip tests for travis deployment
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -5,5 +5,5 @@ echo "tag : $TRAVIS_TAG"
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ] && [ "$TRAVIS_TAG" = '' ]; then
 openssl aes-256-cbc -K $encrypted_30d9879477f5_key -iv $encrypted_30d9879477f5_iv -in etc/codesigning.asc.enc -out etc/codesigning.asc -d
 gpg --fast-import etc/codesigning.asc
-mvn dokka:javadocJar deploy -Dtravis --settings etc/deploy-settings.xml
+mvn dokka:javadocJar deploy -DskipTests=true -Dtravis --settings etc/deploy-settings.xml
 fi
