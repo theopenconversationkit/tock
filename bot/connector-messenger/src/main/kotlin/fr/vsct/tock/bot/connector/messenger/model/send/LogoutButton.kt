5422e8a12dbcf01698527a4ae9b1f65c39fd6328
NainJaune <louart.alexis@gmail.com>
2018-04-26 15:38:35 +0200

    add equals method to messenger logout button

--- bot/connector-messenger/src/main/kotlin/fr/vsct/tock/bot/connector/messenger/model/send/LogoutButton.kt
+++ bot/connector-messenger/src/main/kotlin/fr/vsct/tock/bot/connector/messenger/model/send/LogoutButton.kt
@@ -32,4 +32,16 @@ class LogoutButton : Button(ButtonType.account_unlink) {
             )
         )
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
+
+
 }
