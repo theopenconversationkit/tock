2cedf5442b6ba58322cd52a8a99a59e3289259ef
Julien Buret <jburet@voyages-sncf.com>
2018-05-06 21:17:03 +0200

    resolves #257 remove logback file from tock
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

---
+++ shared/src/main/kotlin/fr/vsct/tock/shared/LogbackConfigurator.kt
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2017 VSCT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.vsct.tock.shared
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.classic.jul.LevelChangePropagator
+import ch.qos.logback.classic.spi.Configurator
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.rolling.RollingFileAppender
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
+import ch.qos.logback.core.spi.ContextAwareBase
+import ch.qos.logback.core.util.FileSize
+
+/**
+ * Default logback configurator.
+ */
+internal class LogbackConfigurator : ContextAwareBase(), Configurator {
+
+    override fun configure(loggerContext: LoggerContext) {
+        if (booleanProperty("tock.logback.enabled", true)) {
+            val c = context
+
+            loggerContext.addListener(LevelChangePropagator().apply {
+                context = c
+                start()
+            })
+
+            val appender = if (booleanProperty("tock.logback.fileAppender", false)) {
+                RollingFileAppender<ILoggingEvent>().apply {
+                    name = "file"
+                    file = "logFile.log"
+                    rollingPolicy = TimeBasedRollingPolicy<ILoggingEvent>().apply {
+                        fileNamePattern = "logFile.%d{yyyy-MM-dd}.log"
+                        maxHistory = 30
+                        setTotalSizeCap(FileSize.valueOf("3GB"))
+                        context = c
+                        start()
+                    }
+                }
+            } else {
+                ConsoleAppender<ILoggingEvent>().apply {
+                    name = "console"
+                }
+            }
+
+            appender.context = c
+            appender.apply {
+                encoder = PatternLayoutEncoder().apply {
+                    pattern = "%d{yyyy-MM-dd'T'HH:mm:ss.SSS} [%thread] %-5level %logger - %msg%n"
+                    context = c
+                    start()
+                }
+            }
+            appender.start()
+
+            loggerContext.getLogger("org.mongodb.driver").apply {
+                level = Level.INFO
+                isAdditive = false
+                addAppender(appender)
+            }
+
+            loggerContext.getLogger("io.netty").apply {
+                level = Level.INFO
+                isAdditive = false
+                addAppender(appender)
+            }
+
+            loggerContext.getLogger("okhttp3").apply {
+                level = Level.INFO
+                isAdditive = false
+                addAppender(appender)
+            }
+
+            loggerContext.getLogger(Logger.ROOT_LOGGER_NAME).apply {
+                level = Level.DEBUG
+                addAppender(appender)
+            }
+        }
+    }
+}
