178d6264cc7abe5688e7410abcfe797e76db50f5
Sébastien Barre <SBarre.prestataire@voyages-sncf.com>
2019-02-27 15:17:57 +0100

    #390: override default method to enable object comparison in test

--- bot/connector-teams/src/main/kotlin/messages/AbstractTeamsBotMessage.kt
+++ bot/connector-teams/src/main/kotlin/messages/AbstractTeamsBotMessage.kt
@@ -8,4 +8,19 @@ abstract class TeamsBotMessage(val text: String?) : ConnectorMessage {
 
     override val connectorType: ConnectorType
         get() = teamsConnectorType
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is TeamsBotMessage) return false
+
+        if (text != other.text) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return text?.hashCode() ?: 0
+    }
+
+
 }
