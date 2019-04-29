47203c99b8f056b6916c3f8de3df6f56d45dec83
Sébastien Barre <SBarre.prestataire@voyages-sncf.com>
2019-04-29 14:25:43 +0200

    #446 fix defect test

--- bot/connector-teams/src/test/kotlin/auth/JWKHandlerTest.kt
+++ bot/connector-teams/src/test/kotlin/auth/JWKHandlerTest.kt
@@ -2,11 +2,13 @@ package fr.vsct.tock.bot.connector.teams.auth
 
 import fr.vsct.tock.bot.connector.teams.auth.MockServer.getMicrosoftMockServer
 import okhttp3.mockwebserver.MockWebServer
+import org.junit.Before
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -25,14 +27,15 @@ class JWKHandlerTest {
         JWKHandler.setOpenIdMatadataLocation("http://${server.hostName}:${server.port}/")
         JWKHandler.setOpenIdMatadataLocationBotFwkEmulator("http://${server.hostName}:${server.port}/")
 
-        JWKHandler.launchJWKCollector(2000)
+        JWKHandler.launchJWKCollector(3000)
 
         Thread.sleep(500)
     }
 
     /**
      * Here we test the cache key system
-     *
+     * The launch collector is initialized with a duration of 3s.
+     * The fifth request, with a timeout of 3s,
      */
     @Test
     fun testJWKCacheSystem() {
@@ -46,14 +49,7 @@ class JWKHandlerTest {
         assertEquals("GET /.well-known/openid-configuration/ HTTP/1.1", thirdRecordedRequest.requestLine)
         val fourthRequest = server.takeRequest()
         assertEquals("GET / HTTP/1.1", fourthRequest.requestLine)
-        val fifthRequest = server.takeRequest(1, TimeUnit.SECONDS)
-        assertNull(fifthRequest)
-
-        Thread.sleep(2000)
-
-        val sixthRecordedRequest = server.takeRequest()
-        assertEquals("GET /.well-known/openidconfiguration/ HTTP/1.1", sixthRecordedRequest.requestLine)
-        val seventhRecordedResponse = server.takeRequest()
-        assertEquals("GET / HTTP/1.1", seventhRecordedResponse.requestLine)
+        val fifthRequest = server.takeRequest(3, TimeUnit.SECONDS)
+        assertEquals("GET /.well-known/openidconfiguration/ HTTP/1.1", fifthRequest.requestLine)
     }
 }
