fab7e8ecf51f016763e60e1be33f96bf83b68ac7
Sébastien Barre <sbarre.prestataire@oui.sncf>
2019-05-29 10:57:39 +0200

    Add generic message to teams cards

--- bot/connector-teams/src/main/kotlin/messages/TeamsBotTextMessage.kt
+++ bot/connector-teams/src/main/kotlin/messages/TeamsBotTextMessage.kt
@@ -2,6 +2,12 @@ package fr.vsct.tock.bot.connector.teams.messages
 
 import com.microsoft.bot.schema.models.CardAction
 import com.microsoft.bot.schema.models.CardImage
+import fr.vsct.tock.bot.connector.teams.teamsConnectorType
+import fr.vsct.tock.bot.engine.action.SendAttachment
+import fr.vsct.tock.bot.engine.message.Attachment
+import fr.vsct.tock.bot.engine.message.Choice
+import fr.vsct.tock.bot.engine.message.GenericMessage
+import fr.vsct.tock.shared.mapNotNullValues
 
 class TeamsBotTextMessage(text: String) : TeamsBotMessage(text) {
     override fun equals(other: Any?): Boolean {
@@ -13,6 +19,13 @@ class TeamsBotTextMessage(text: String) : TeamsBotMessage(text) {
 
     override fun toString(): String {
         return text ?: ""
+    }
+
+    override fun toGenericMessage(): GenericMessage {
+        return GenericMessage(
+            connectorType = connectorType,
+            texts = mapOf("text" to toString())
+        )
     }
 }
 
@@ -37,11 +50,58 @@ class TeamsHeroCard(
             "tap=${tap?.value()})"
     }
 
+    override fun toGenericMessage(): GenericMessage {
+        return GenericMessage(
+            connectorType = connectorType,
+            texts = mapNotNullValues(
+                "title" to title,
+                "subtitle" to subtitle,
+                "attachmentContent" to attachmentContent
+            ),
+            choices = buttons?.map {
+                Choice(intentName = it.text()?.toString() ?: it.title(),
+                    parameters = mapNotNullValues(
+                        "title" to it.title()?.toString(),
+                        "value" to it.value()?.toString(),
+                        "displayText" to it.displayText(),
+                        "text" to it.text(),
+                        "type" to it.type().toString()
+                    )
+                )
+            } ?: emptyList(),
+            attachments = images?.map {
+                Attachment(
+                    url = it.url(),
+                    type = SendAttachment.AttachmentType.image
+                )
+            } ?: emptyList()
+        )
+    }
+
 }
 
 class TeamsCardAction(
     val actionTitle: String,
     val buttons: List<CardAction>): TeamsBotMessage(null) {
+
+    override fun toGenericMessage(): GenericMessage? {
+        return GenericMessage(
+            connectorType = connectorType,
+            texts = mapNotNullValues("title" to actionTitle),
+            choices = buttons.map {
+                Choice(intentName = it.text()?.toString() ?: it.title(),
+                    parameters = mapNotNullValues(
+                        "title" to it.title()?.toString(),
+                        "value" to it.value()?.toString(),
+                        "displayText" to it.displayText(),
+                        "text" to it.text(),
+                        "type" to it.type().toString()
+                    )
+                )
+            }
+        )
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is TeamsCardAction) return false
