9da6b5d0a47943ab432fbc41a4e8f8ff2541295e
germain_soumier <gsoumier.prestataire@oui.sncf>
2019-07-05 19:24:31 +0200

    [TESTING] add bot fluent assertions

---
+++ bot/test-base/src/main/kotlin/fr/vsct/tock/bot/test/BotBusMessengerAsserts.kt
@@ -0,0 +1,64 @@
+package fr.vsct.tock.bot.test
+
+
+import ch.tutteli.atrium.api.cc.en_GB.isA
+import ch.tutteli.atrium.api.cc.en_GB.notToBeNull
+import ch.tutteli.atrium.api.cc.en_GB.property
+import ch.tutteli.atrium.api.cc.en_GB.toBe
+import ch.tutteli.atrium.creating.Assert
+import ch.tutteli.atrium.domain.builders.AssertImpl
+import ch.tutteli.atrium.domain.creating.any.typetransformation.AnyTypeTransformation
+import ch.tutteli.atrium.reporting.RawString
+import ch.tutteli.atrium.reporting.translating.Untranslatable
+import ch.tutteli.atrium.verbs.expect
+import fr.vsct.tock.bot.connector.messenger.model.send.Attachment
+import fr.vsct.tock.bot.connector.messenger.model.send.AttachmentMessage
+import fr.vsct.tock.bot.connector.messenger.model.send.ButtonPayload
+import fr.vsct.tock.bot.connector.messenger.model.send.PostbackButton
+import fr.vsct.tock.bot.connector.messenger.model.send.TextMessage
+import fr.vsct.tock.bot.connector.messenger.model.send.TextQuickReply
+import fr.vsct.tock.bot.connector.messenger.model.send.UrlButton
+
+
+fun Assert<BotBusMockLog>.toBeMessengerTextMessage(text: String) {
+    expect(subject.messenger()).notToBeNull { isA<TextMessage> { property(TextMessage::text).toBe(text) } }
+}
+
+fun Assert<BotBusMockLog>.toBeMessengerAttachmentMessage(assertionCreator: Assert<AttachmentMessage>.() -> Unit) {
+    val parameterObject = AnyTypeTransformation.ParameterObject(
+        Untranslatable("is a"),
+        RawString.create(AttachmentMessage::class.simpleName!!),
+        this,
+        assertionCreator,
+        Untranslatable("is not an messenger attachment message")
+    )
+    AssertImpl.any.typeTransformation.transform(
+        parameterObject, { it.messenger() is AttachmentMessage }, { it.messenger() as AttachmentMessage },
+        AssertImpl.any.typeTransformation.failureHandlers.newExplanatory()
+    )
+}
+
+fun Assert<AttachmentMessage>.withButtonAttachment(text: String, buttonTitle: String) =
+    withButtonAttachment(text, listOf(buttonTitle))
+
+fun Assert<AttachmentMessage>.withButtonAttachment(text: String, buttonTitles: List<String>) =
+    property(AttachmentMessage::attachment)
+        .property(Attachment::payload)
+        .isA<ButtonPayload> {
+            property(ButtonPayload::text).toBe(text)
+            property(ButtonPayload::buttons) {
+                expect(subject.map { button ->
+                    when (button) {
+                        is PostbackButton -> (button as PostbackButton).title
+                        is UrlButton -> (button as UrlButton).title
+                        else -> null
+                    }
+                }).toBe(buttonTitles)
+
+            }
+        }
+
+fun Assert<AttachmentMessage>.withTextQuickReplies(quickReplies: List<String>) = addAssertionsCreatedBy {
+    expect(subject.quickReplies.orEmpty().filterIsInstance<TextQuickReply>().map { it.title })
+        .toBe(quickReplies)
+}
