8c0ceca6c9ebc6ce3b263f4b69ca884434a2e66c
Julien Buret <jburet@voyages-sncf.com>
2019-09-11 23:37:45 +0200

    add milestone builds
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -3,8 +3,17 @@ echo "branch : $TRAVIS_BRANCH"
 echo "pull_request : $TRAVIS_PULL_REQUEST"
 echo "tag : $TRAVIS_TAG"
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ] && [ "$TRAVIS_TAG" = '' ]; then
-openssl aes-256-cbc -K $encrypted_30d9879477f5_key -iv $encrypted_30d9879477f5_iv -in etc/codesigning.asc.enc -out etc/codesigning.asc -d
-gpg --fast-import etc/codesigning.asc
-mvn deploy -DskipTests=true -Dtravis --settings etc/deploy-settings.xml -U
-#mvn dokka:javadocJar deploy -DskipTests=true -Dtravis --settings etc/deploy-settings.xml
+  openssl aes-256-cbc -K $encrypted_30d9879477f5_key -iv $encrypted_30d9879477f5_iv -in etc/codesigning.asc.enc -out etc/codesigning.asc -d
+  gpg --fast-import etc/codesigning.asc
+  if [ "$TOCK_MILESTONE" = '' ];
+  then
+    mvn deploy -DskipTests=true -Dtravis --settings etc/deploy-settings.xml -U
+  else
+    export TOCK_VERSION=$(cat pom.xml | grep "^    <version>.*</version>$" | awk -F'[><]' '{print $3}')
+    echo "tock version : $TOCK_VERSION"
+    export TOCK_TAG=${TOCK_VERSION%"-SNAPSHOT"}
+    echo "tock tag : build-$TOCK_TAG-$TRAVIS_BUILD_NUMBER"
+    mvn versions:set -DnewVersion="build-$TOCK_TAG-$TRAVIS_BUILD_NUMBER"
+    mvn deploy -Dmilestone --settings etc/deploy-settings.xml
+  fi
 fi
