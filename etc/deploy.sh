b6bddd7118dfed1864fb7c92ebbd53e11a4aece8
Julien Buret <jburet@voyages-sncf.com>
2019-10-16 19:10:16 +0200

    allow building on build tag
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -2,7 +2,7 @@
 echo "branch : $TRAVIS_BRANCH"
 echo "pull_request : $TRAVIS_PULL_REQUEST"
 echo "tag : $TRAVIS_TAG"
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
+if [[ $TRAVIS_BRANCH = *"build"* || $TRAVIS_BRANCH = "master" ]] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
   openssl aes-256-cbc -K $encrypted_30d9879477f5_key -iv $encrypted_30d9879477f5_iv -in etc/codesigning.asc.enc -out etc/codesigning.asc -d
   gpg --fast-import etc/codesigning.asc
   if [ "$TRAVIS_TAG" = '' ];
