abf6dcf49ab925ee35f2cb08c7d5be1b946439cf
Julien Buret <jburet@voyages-sncf.com>
2019-11-07 13:26:48 +0100

    update copyright
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

--- bot/api/retrofit-jackson-client/src/main/kotlin/RetrofitJackson.kt
+++ bot/api/retrofit-jackson-client/src/main/kotlin/RetrofitJackson.kt
@@ -1,4 +1,30 @@
 /*
+ * Copyright (C) 2017/2019 e-voyageurs technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.tock.bot.api.retrofit
+
+import ai.tock.shared.jackson.mapper
+import ai.tock.shared.longProperty
+import okhttp3.OkHttpClient.Builder
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
+import java.util.concurrent.TimeUnit.MILLISECONDS
+
+/*
  * Copyright (C) 2017/2019 VSCT
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
