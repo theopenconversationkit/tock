1f952e62dfa35a5d42a7b468cfc864eded799b0b
Julien Buret <jburet@voyages-sncf.com>
2020-03-05 10:55:43 +0100

    messenger: remove deprecated tags [2]
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

--- bot/connector-messenger/src/main/kotlin/model/send/MessageTag.kt
+++ bot/connector-messenger/src/main/kotlin/model/send/MessageTag.kt
@@ -21,26 +21,6 @@ import ai.tock.bot.engine.action.ActionNotificationType
 
 
 enum class MessageTag {
-    @Deprecated("Available until January 15th, 2020 Partially covered as the new POST_PURCHASE_UPDATE")
-    SHIPPING_UPDATE,
-    @Deprecated("Available until January 15th, 2020 Partially covered as the new tag POST_PURCHASE_UPDATE")
-    RESERVATION_UPDATE,
-    @Deprecated("Available until January 15th, 2020 Partially covered as the new tag HUMAN_AGENT")
-    ISSUE_RESOLUTION,
-    @Deprecated("Available until January 15th, 2020 Partially covered as the new tag CONFIRMED_EVENT_UPDATE")
-    APPOINTMENT_UPDATE,
-    @Deprecated("No longer supported after January 15, 2020")
-    GAME_EVENT,
-    @Deprecated("Available until January 15th, 2020 Partially covered as the new tag CONFIRMED_EVENT_UPDATE")
-    TRANSPORTATION_UPDATE,
-    @Deprecated("Available until January 15th, 2020 Partially covered as the new tag HUMAN_AGENT")
-    FEATURE_FUNCTIONALITY_UPDATE,
-    @Deprecated("Available until January 15th, 2020 Partially covered as the new tag CONFIRMED_EVENT_UPDATE")
-    TICKET_UPDATE,
-    @Deprecated("Available until January 15th, 2020 Partially covered as the new tag POST_PURCHASE_UPDATE")
-    PAYMENT_UPDATE,
-    @Deprecated("Available until January 15th, 2020 Partially covered as the new tag ACCOUNT_UPDATE")
-    PERSONAL_FINANCE_UPDATE,
 
     CONFIRMED_EVENT_UPDATE,
     POST_PURCHASE_UPDATE,
@@ -50,12 +30,6 @@ enum class MessageTag {
     companion object {
         fun toMessageTag(action: Action): MessageTag? {
             return when (action.metadata.notificationType) {
-                ActionNotificationType.transportationUpdate -> TRANSPORTATION_UPDATE
-                ActionNotificationType.issueResolution -> ISSUE_RESOLUTION
-                ActionNotificationType.newFeatureFunctionality -> FEATURE_FUNCTIONALITY_UPDATE
-                ActionNotificationType.reservationUpdate -> RESERVATION_UPDATE
-                ActionNotificationType.paymentUpdate -> PAYMENT_UPDATE
-                ActionNotificationType.personalFinanceUpdate -> PERSONAL_FINANCE_UPDATE
                 ActionNotificationType.confirmedEventUpdate -> CONFIRMED_EVENT_UPDATE
                 ActionNotificationType.humanAgent -> HUMAN_AGENT
                 ActionNotificationType.postPurchaseUpdate -> POST_PURCHASE_UPDATE
