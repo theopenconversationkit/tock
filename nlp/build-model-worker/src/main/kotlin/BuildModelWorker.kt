4a3632df5b02f55b0d5418d457aa58db8ea30bf8
Erwan LE BESCOND <elebescond@gmail.com>
2020-03-06 15:39:15 +0100

    fixes #808 : Running worker tasks with an ending process

---
+++ nlp/build-model-worker/src/main/kotlin/BuildModelWorker.kt
@@ -0,0 +1,211 @@
+/*
+ * Copyright (C) 2017/2020 e-voyageurs technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.tock.nlp.build
+
+import ai.tock.nlp.front.client.FrontClient
+import ai.tock.nlp.front.shared.build.ModelBuildTrigger
+import ai.tock.nlp.front.shared.config.ApplicationDefinition
+import ai.tock.nlp.front.shared.config.ClassifiedSentence
+import ai.tock.nlp.front.shared.config.ClassifiedSentenceStatus.deleted
+import ai.tock.nlp.front.shared.config.ClassifiedSentenceStatus.model
+import ai.tock.nlp.front.shared.config.ClassifiedSentenceStatus.validated
+import ai.tock.shared.booleanProperty
+import ai.tock.shared.error
+import ai.tock.shared.namespace
+import mu.KotlinLogging
+import org.litote.kmongo.Id
+import java.util.*
+
+/**
+ *
+ */
+class BuildModelWorker {
+
+    data class ModelRefreshKey(val applicationId: Id<ApplicationDefinition>, val language: Locale)
+
+    companion object {
+        private val logger = KotlinLogging.logger {}
+        private val testModelEnabled = booleanProperty("tock_test_model_enabled", true)
+        internal val cleanupModelEnabled = booleanProperty("tock_cleanup_model_enabled", true)
+
+        val front = FrontClient
+
+        fun updateAllModels() {
+            FrontClient.getApplications().forEach { updateApplicationModels(it) }
+        }
+
+        private fun updateApplicationModels(app: ApplicationDefinition, onlyIfNotExists: Boolean = false) {
+            logger.info { "Rebuild all models for application ${app.name} and nlp engine ${app.nlpEngineType.name}" }
+            app.supportedLocales.forEach { locale ->
+                front.updateIntentsModelForApplication(emptyList(), app, locale, app.nlpEngineType, onlyIfNotExists)
+                app.intents.forEach { intentId ->
+                    logger.info { "start model update for ${app.name}, intent ${intentId} and $locale" }
+                    front.updateEntityModelForIntent(
+                        emptyList(),
+                        app,
+                        intentId,
+                        locale,
+                        app.nlpEngineType,
+                        onlyIfNotExists
+                    )
+                }
+                updateEntityModels(app, locale, emptyList(), onlyIfNotExists)
+            }
+        }
+
+        private fun updateModel(
+            key: ModelRefreshKey,
+            sentences: List<ClassifiedSentence>,
+            onlyIfNotExists: Boolean = false
+        ) {
+            try {
+                val app = front.getApplicationById(key.applicationId)
+                if (app == null) {
+                    logger.warn { "Unknown application : $key" }
+                    return
+                }
+                logger.info { "Start model update for ${app.name} and ${key.language}" }
+                logger.trace { "New sentences : ${sentences.map { it.text }}" }
+
+                front.updateIntentsModelForApplication(sentences, app, key.language, app.nlpEngineType, onlyIfNotExists)
+                sentences.groupBy { it.classification.intentId }.forEach { intentId, intentSentences ->
+                    logger.info { "start model update for ${app.name}, intent ${intentId} and ${key.language}" }
+                    front.updateEntityModelForIntent(
+                        intentSentences,
+                        app,
+                        intentId,
+                        key.language,
+                        app.nlpEngineType,
+                        onlyIfNotExists
+                    )
+                }
+
+                updateEntityModels(app, key.language, sentences, onlyIfNotExists)
+
+                logger.info { "Model updated for ${app.name} and ${key.language}" }
+            } catch (e: Throwable) {
+                logger.error(e)
+            } finally {
+                logger.info { "end model update for ${key.language}" }
+                front.switchSentencesStatus(sentences, model)
+            }
+        }
+
+        private fun updateEntityModels(
+            app: ApplicationDefinition,
+            locale: Locale,
+            sentences: List<ClassifiedSentence>,
+            onlyIfNotExists: Boolean = false
+        ) {
+            front.getEntityTypes()
+                .filter { it.subEntities.isNotEmpty() }
+                .filter { it.name.namespace() == app.namespace }
+                .forEach { entityType ->
+                    val sentencesWithEntity = sentences.filter { s -> s.classification.containsEntityOrSubEntity(entityType.name) }
+                    if (sentencesWithEntity.isNotEmpty()) {
+                        logger.info { "start model update for ${app.name}, entity type $entityType and $locale" }
+                        front.updateEntityModelForEntityType(
+                            sentencesWithEntity,
+                            app,
+                            entityType,
+                            locale,
+                            app.nlpEngineType,
+                            onlyIfNotExists
+                        )
+                    }
+                }
+
+        }
+
+        fun buildModelWithValidatedSentences(): Boolean {
+            val validatedSentences: List<ClassifiedSentence> = front.getSentences(status = validated)
+            if (validatedSentences.isNotEmpty()) {
+                logger.debug { "Sentences to update : ${validatedSentences.map { it.text }}" }
+
+                val refreshKeyMap =
+                    validatedSentences.groupBy { ModelRefreshKey(it.applicationId, it.language) }
+                logger.info { "Model refresh keys : ${refreshKeyMap.keys}" }
+                refreshKeyMap.forEach {
+                    updateModel(it.key, it.value)
+                }
+                logger.info { "end model update" }
+                return true
+            }
+            return false
+        }
+
+        fun buildModelWithDeletedSentences(): Boolean {
+            val deletedSentences: List<ClassifiedSentence> = front.getSentences(status = deleted)
+            if (deletedSentences.isNotEmpty()) {
+                logger.debug { "Sentences to remove from model : ${deletedSentences.map { it.text }}" }
+
+                val refreshKeyMap =
+                    deletedSentences.map { ModelRefreshKey(it.applicationId, it.language) }.distinct()
+                logger.info { "Model refresh keys : $refreshKeyMap" }
+                refreshKeyMap.forEach {
+                    updateModel(it, emptyList())
+                }
+                front.deleteSentencesByStatus(deleted)
+                logger.info { "end model update" }
+                return true
+            }
+            return false
+        }
+
+        fun buildModelForTriggeredApplication(): Boolean {
+            val triggers: List<ModelBuildTrigger> = front.getTriggers()
+            if (triggers.isNotEmpty()) {
+                triggers[0].let { trigger ->
+                    logger.info { "use trigger $trigger" }
+                    front.deleteTrigger(trigger)
+                    front.getApplicationById(trigger.applicationId)?.let {
+                        updateApplicationModels(it, trigger.onlyIfModelNotExists)
+                    } ?: logger.warn {
+                        "unknown application id trigger ${trigger} - skipped"
+                    }
+                }
+                return true
+            }
+            return false
+        }
+
+        fun testModels(): Boolean {
+            if (testModelEnabled) {
+                logger.info { "Start testing models" }
+                front.testModels()
+                return true
+            }
+            return false
+        }
+
+        fun completeModel() {
+            logger.info { "trigger build to check not existing models" }
+            front.getApplications().forEach {
+                front.triggerBuild(ModelBuildTrigger(it._id, true, true))
+            }
+        }
+
+        fun cleanupModel() {
+            if(cleanupModelEnabled) {
+                logger.debug { "remove orphan models..." }
+                front.deleteOrphans()
+                logger.debug { "end remove orphan models" }
+            }
+        }
+    }
+
+}
--- nlp/build-model-worker/src/main/kotlin/startBuildWorker.kt
+++ nlp/build-model-worker/src/main/kotlin/startBuildWorker.kt
@@ -16,20 +16,82 @@
 
 package ai.tock.nlp.build
 
-import com.github.salomonbrys.kodein.Kodein
+import ai.tock.nlp.build.BuildType.CLEANUP
+import ai.tock.nlp.build.BuildType.REBUILD_ALL
+import ai.tock.nlp.build.BuildType.REBUILD_DIFF
+import ai.tock.nlp.build.BuildType.TEST
 import ai.tock.nlp.front.ioc.FrontIoc
+import ai.tock.shared.booleanProperty
+import ai.tock.shared.error
 import ai.tock.shared.vertx.vertx
+import com.github.salomonbrys.kodein.Kodein
 import io.vertx.core.DeploymentOptions
+import mu.KotlinLogging
+import kotlin.system.exitProcess
 
-fun main(args: Array<String>) {
-    startBuildWorker()
+private val logger = KotlinLogging.logger {}
+private val buildWorkerVerticleEnabled = booleanProperty("tock_build_worker_verticle_enabled", true)
+
+/**
+ * Build type.
+ */
+enum class BuildType {
+    /**
+     * Build is started only if at least one sentence status has changed.
+     */
+    REBUILD_DIFF,
+
+    /**
+     * Full rebuild.
+     */
+    REBUILD_ALL,
+
+    /**
+     * Test build.
+     */
+    TEST,
+
+    /**
+     * Cleanup orphan builds.
+     */
+    CLEANUP
 }
 
-fun startBuildWorker(vararg modules: Kodein.Module) {
+fun main(vararg args: String) {
+    startBuildWorker(args.getOrNull(0)?.let { arg -> BuildType.values().find { it.name == arg } }
+        ?: REBUILD_ALL)
+}
+
+fun startBuildWorker(buildType: BuildType, vararg modules: Kodein.Module) {
     FrontIoc.setup(*modules)
+    if (buildWorkerVerticleEnabled) {
+        startVerticle()
+    } else {
+        startProcess(buildType)
+    }
+}
+
+private fun startVerticle() {
     val buildModelWorkerVerticle = BuildModelWorkerVerticle()
     vertx.deployVerticle(buildModelWorkerVerticle, DeploymentOptions().setWorker(true))
     vertx.deployVerticle(CleanupModelWorkerVerticle(), DeploymentOptions().setWorker(true))
     vertx.deployVerticle(HealthCheckVerticle(buildModelWorkerVerticle))
 }
 
+private fun startProcess(buildType: BuildType) {
+    try {
+        when (buildType) {
+            REBUILD_ALL -> BuildModelWorker.updateAllModels()
+            REBUILD_DIFF -> {
+                BuildModelWorker.buildModelWithValidatedSentences()
+                BuildModelWorker.buildModelWithDeletedSentences()
+            }
+            TEST -> BuildModelWorker.testModels()
+            CLEANUP -> BuildModelWorker.cleanupModel()
+        }
+        exitProcess(0)
+    } catch (e: Throwable) {
+        logger.error(e)
+        exitProcess(1)
+    }
+}
