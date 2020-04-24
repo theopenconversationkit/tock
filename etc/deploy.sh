d9280c6b2d924198dafb95178988f9eda1cdc607
Julien Buret <jburet@voyages-sncf.com>
2020-04-24 23:36:36 +0200

    speed up build by parallelizing
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -7,7 +7,7 @@ if [[ $TRAVIS_BRANCH = *"build"* || $TRAVIS_BRANCH = "master" ]] && [ "$TRAVIS_P
   gpg --fast-import etc/codesigning.asc
   if [ "$TRAVIS_TAG" = '' ];
   then
-    mvn test -B -Dskip.npm -Dassembly.skipAssembly=true -U -q || exit 1
+    mvn test -B -Dskip.npm -Dassembly.skipAssembly=true -T 2C -U -q || exit 1
     if [ "$SKIP_DEPLOY" != 'true' ]; then
       mvn -B deploy -T 4 -DskipTests=true -Dtravis --settings etc/deploy-settings.xml -U -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
     fi
@@ -18,16 +18,16 @@ if [[ $TRAVIS_BRANCH = *"build"* || $TRAVIS_BRANCH = "master" ]] && [ "$TRAVIS_P
       echo "tock version : $TOCK_VERSION"
       echo "tock tag : $TRAVIS_TAG"
       mvn versions:set -DnewVersion="$TRAVIS_TAG"
-      mvn install -B -U -q || exit 1
+      mvn install -T 2C -B -U -q || exit 1
       mvn deploy -B -T 4 -DskipTests=true -Dmilestone --settings etc/deploy-settings.xml -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
     fi
   fi
   else
     if [ "$TRAVIS_PULL_REQUEST" = 'false' ];
     then
-      mvn test -B -Dskip.npm -Dassembly.skipAssembly=true -q
+      mvn test -T 2C -B -Dskip.npm -Dassembly.skipAssembly=true -q
     else
       echo "test PR : $TRAVIS_PULL_REQUEST"
-      mvn clean install -B -Dskip.npm -Dassembly.skipAssembly=true -q
+      mvn clean install -T 2C -B -Dskip.npm -Dassembly.skipAssembly=true -q
     fi
 fi
