4db10a334d03d0a91ec1b216bdc35d0b00fd3f4d
Julien Buret <jburet@voyages-sncf.com>
2020-09-28 13:01:39 +0200

    update dependencies
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

--- bot/engine/src/test/kotlin/connector/ConnectorQueueTest.kt
+++ bot/engine/src/test/kotlin/connector/ConnectorQueueTest.kt
@@ -25,9 +25,9 @@ internal class ConnectorQueueTest {
         val action3b = `given action`("user1")
         val send = spyk({ action: Action -> })
         send(action1a)
-        queue.add(action1b, 0, { action -> Thread.sleep(1000); send(action); })
+        queue.add(action1b, 0) { action -> Thread.sleep(1000); send(action); }
         send(action2a)
-        queue.add(action2b, 0, { action -> Thread.sleep(500); send(action); })
+        queue.add(action2b, 0) { action -> Thread.sleep(500); send(action); }
         send(action3a)
         queue.add(action3b, 0, send)
         verify(timeout = 5000, ordering = Ordering.SEQUENCE) {
@@ -42,21 +42,25 @@ internal class ConnectorQueueTest {
 
     @Test
     fun `each user has their own queue which keeps the order in which messages are sent`() {
-        val queue = ConnectorQueue(executor)
-        val user1Action1 = `given action`("user1")
-        val user1Action2 = `given action`("user1")
-        val user2Action1 = `given action`("user2")
-        val user2Action2 = `given action`("user2")
-        val send = spyk({ action: Action -> })
-        queue.add(user1Action1, 0, { action -> Thread.sleep(1500); send(action); })
-        queue.add(user1Action2, 0, { action -> Thread.sleep(1000); send(action); })
-        queue.add(user2Action1, 0, { action -> Thread.sleep(500); send(action); })
-        queue.add(user2Action2, 0, { action -> Thread.sleep(0); send(action); })
-        verify(timeout = 5000, ordering = Ordering.SEQUENCE) {
-            send(user2Action1)
-            send(user2Action2)
-            send(user1Action1)
-            send(user1Action2)
+        try {
+            val queue = ConnectorQueue(executor)
+            val user1Action1 = `given action`("user1")
+            val user1Action2 = `given action`("user1")
+            val user2Action1 = `given action`("user2")
+            val user2Action2 = `given action`("user2")
+            val send = spyk({ _: Action -> })
+            queue.add(user1Action1, 0) { action -> Thread.sleep(1500); send(action); }
+            queue.add(user1Action2, 0) { action -> Thread.sleep(1000); send(action); }
+            queue.add(user2Action1, 0) { action -> Thread.sleep(500); send(action); }
+            queue.add(user2Action2, 0) { action -> Thread.sleep(0); send(action); }
+            verify(timeout = 5000, ordering = Ordering.SEQUENCE) {
+                send(user2Action1)
+                send(user2Action2)
+                send(user1Action1)
+                send(user1Action2)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
