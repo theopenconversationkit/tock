3ff9b9dc8c6211d33e976da9abdf48abb7235464
rkuffer <101799433+rkuffer@users.noreply.github.com>
2022-04-26 11:46:10 +0200

    Comment out unused "onPdfAction" methods importing non esm module "html2pdf"

--- bot/admin/web/src/app/analytics/activity/activity.component.ts
+++ bot/admin/web/src/app/analytics/activity/activity.component.ts
@@ -15,7 +15,7 @@
  */
 
 import { Component, OnInit } from '@angular/core';
-import * as html2pdf from 'html2pdf.js';
+// import * as html2pdf from 'html2pdf.js';
 import { StateService } from 'src/app/core-nlp/state.service';
 import { BotConfigurationService } from 'src/app/core/bot-configuration.service';
 import { BotApplicationConfiguration, ConnectorType } from 'src/app/core/model/configuration';
@@ -156,16 +156,16 @@ export class ActivityComponent implements OnInit {
     return fileName;
   }
 
-  onPdfAction() {
-    const options = {
-      filename: this.getFileName(),
-      image: { type: 'jpeg ', quality: 0.95 },
-      html2canvas: {},
-      jsPDF: { orientation: 'landscape' }
-    };
-    const content: Element = document.getElementById('element-id');
-    html2pdf().from(content).set(options).save();
-  }
+  // onPdfAction() {
+  //   const options = {
+  //     filename: this.getFileName(),
+  //     image: { type: 'jpeg ', quality: 0.95 },
+  //     html2canvas: {},
+  //     jsPDF: { orientation: 'landscape' }
+  //   };
+  //   const content: Element = document.getElementById('element-id');
+  //   html2pdf().from(content).set(options).save();
+  // }
 
   getConnector(connectorId: string): ConnectorType {
     let connectors = this.configurations
