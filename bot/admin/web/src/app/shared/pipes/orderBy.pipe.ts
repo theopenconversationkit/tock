42fb41f2b74c1153d4146e005bae85249a52410d
rkuffer <101799433+rkuffer@users.noreply.github.com>
2022-05-05 17:34:47 +0200

    Scenarios simple list with sortable columns

--- bot/admin/web/src/app/shared/pipes/orderBy.pipe.ts
+++ bot/admin/web/src/app/shared/pipes/orderBy.pipe.ts
@@ -4,23 +4,20 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'orderBy'
 })
 export class OrderByPipe implements PipeTransform {
-  transform(array: any, field: string): any[] {
-    if (!Array.isArray(array)) {
-      throw new TypeError('Invalid array argument');
-    }
-
-    if (!field) {
-      throw new Error('The field parameter cannot be empty');
+  transform(array: any, field: string, reverse: boolean = false, secondField?: string): any[] {
+    if (!Array.isArray(array) || !field) {
+      return array;
     }
 
     array.sort((a: any, b: any) => {
-      if (!a[field] && !b[field]) return 0;
+      if (!a[field] || !b[field]) return 0;
 
-      if (a[field].toUpperCase() < b[field].toUpperCase()) return -1;
-      else if (a[field].toUpperCase() > b[field].toUpperCase()) return 1;
-      else return 0;
+      if (secondField) {
+        return a[field].localeCompare(b[field]) || a[secondField].localeCompare(b[secondField]);
+      }
+      return a[field].localeCompare(b[field]);
     });
 
-    return array;
+    return reverse ? array.reverse() : array;
   }
 }
