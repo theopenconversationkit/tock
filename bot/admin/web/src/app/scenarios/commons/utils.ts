fd15f8df3698a6028a4bc7b02d99ca80a2d6c1fc
rkuffer <101799433+rkuffer@users.noreply.github.com>
2022-07-05 11:14:39 +0200

    feat(scenario-designer): UI consistency. Context entities display

--- bot/admin/web/src/app/scenarios/commons/utils.ts
+++ bot/admin/web/src/app/scenarios/commons/utils.ts
@@ -7,3 +7,13 @@ export function normalizedSnakeCase(str: string): string {
     .replace(/\s+/g, '_')
     .replace(/[^A-Za-z0-9_]*/g, '');
 }
+
+export function getContrastYIQ(hexcolor) {
+  if (!hexcolor) return '';
+  hexcolor = hexcolor.replace('#', '');
+  var r = parseInt(hexcolor.substr(0, 2), 16);
+  var g = parseInt(hexcolor.substr(2, 2), 16);
+  var b = parseInt(hexcolor.substr(4, 2), 16);
+  var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  return yiq >= 128 ? 'black' : 'white';
+}
