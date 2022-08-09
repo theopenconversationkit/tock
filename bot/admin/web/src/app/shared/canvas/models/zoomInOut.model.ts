3f61cfc2689503e8bfd0eddd53ae8f64a627a6ca
rkuffer <101799433+rkuffer@users.noreply.github.com>
2022-08-09 10:04:04 +0200

    fix(scenario-designer): css syntax + initial zoom ratio

--- bot/admin/web/src/app/shared/canvas/models/zoomInOut.model.ts
+++ bot/admin/web/src/app/shared/canvas/models/zoomInOut.model.ts
@@ -12,5 +12,5 @@ export interface ActionZoomEvent {
 export const CANVAS_TRANSITION_TIMING = 300;
 export const ZOOM_SPEED = 0.5;
 export const ZOOM_SCALE = 0.75;
-export const MAX_SCALE = 1.1;
+export const MAX_SCALE = 1;
 export const MIN_SCALE = 0.2;
