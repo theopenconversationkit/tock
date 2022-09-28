d2bfcadd9136a1e2839fee3745551ee5262633ec
david hamelin <93144560+davhamelin@users.noreply.github.com>
2022-09-28 15:17:07 +0200

    contribution/iadvize (#1394)
    * add models and firsts route for connector iadvize
    
    * first message fonctional (in loop)
    
    * connector iadvize functional
    
    * work
    
    * fix(bot): empty path redirect on bot.module
    
    * fix(nlp): empty path redirect on nlp.module
    
    * fix(nlp): empty path redirect on quality.module
    
    * fix(nlp): share the getApplications observable between the successive subscriptions
    
    * fix(bot): empty path redirect on test.module
    
    * fix(bot): share the getConfiguration observable between the successive subscriptions
    
    * fix(bot): getConfigurationPending variable typing
    
    * add TU
    
    * fix(bot): remove deprecated node-sass dependency
    
    * fix(nlp): remove deprecated node-sass dependency
    
    * corrections suite à relecture
    
    * upgrade to 21.9.7-snapshot
    
    * correction d'un TU impacté par effet de bord d'une modification de l'engin pour le connecteur iAdvize
    
    * Fix errors on dialog closing
    
    * add card and quick reply, TU implemented, but not tested
    
    * add iAdvize builder
    
    * fix commentary
    
    * revert: put back master without diff changes (6295e6ca4f5b1c1fdd45dd16b07e27e04753ef41 and 789fa7153a2356daaa7bf5bf6618e482bbecb41d)
    
    * revert: put back master without diff changes 9d6d6923c4412f3921b59117e95ff72566623aee
    
    * remove cli analytics
    
    * groupe try catch on iadvize connector
    
    * correct commentary
    
    * jordan review
    
    * explaination
    
    * taking acount of review
    
    * remove dead code
    
    * fix bad response format
    
    * add methode to use quick reply with story configured and iadvize
    
    * fix bug on echo response by operator role
    
    * try to fix 292 but it's already the solution implemeted
    
    * correction of commentary
    
    * Iadvize tranfer to a human [DERBOT-286] (#100)
    
    add transfer to humain on iadvize connector
    
    * update version for iadvize
    
    * update connector iadvize to be used in bot api mode
    
    * tock review
    
    Co-authored-by: rkuffer <101799433+rkuffer@users.noreply.github.com>
    Co-authored-by: djowalker <jordan.monfort@partnre.com>
    Co-authored-by: SECHET Brandon <brandon.sechet@partnre.com>

---
+++ bot/connector-iadvize/src/main/kotlin/model/request/UpdateBot.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2017/2022 e-voyageurs technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.tock.bot.connector.iadvize.model.request
+
+import ai.tock.bot.connector.iadvize.model.DistributionRule
+
+data class UpdateBot(
+    val name: String,
+    val pseudo: String,
+    val language: String,
+    val distributionRules: List<DistributionRule>,
+    val external: BotId
+) {
+    data class BotId(val idBot: String)
+}
