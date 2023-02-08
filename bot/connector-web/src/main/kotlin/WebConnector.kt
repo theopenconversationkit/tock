80dbd24c79b868f06fb06a5d974da0091dc11146
Julien Buret <jburet@voyages-sncf.com>
2023-02-08 19:09:00 +0100

    fixes #1447 Locale choice is not persisted for web connector

--- bot/connector-web/src/main/kotlin/WebConnector.kt
+++ bot/connector-web/src/main/kotlin/WebConnector.kt
@@ -71,7 +71,7 @@ import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.CorsHandler
 import mu.KotlinLogging
 import java.time.Duration
-import java.util.*
+import java.util.UUID
 
 internal const val WEB_CONNECTOR_ID = "web"
 
@@ -91,17 +91,17 @@ private val webConnectorBridgeEnabled = booleanProperty("tock_web_connector_brid
 private val webConnectorExtraHeaders = listProperty("tock_web_connector_extra_headers", emptyList())
 
 class WebConnector internal constructor(
-        val applicationId: String,
-        val path: String
+    val applicationId: String,
+    val path: String
 ) : ConnectorBase(webConnectorType, setOf(CAROUSEL)), OrchestrationConnector {
 
     companion object {
         private val logger = KotlinLogging.logger {}
         private val webMapper = mapper.copy().registerModules(
-                SimpleModule().apply {
-                    // fallback for serializing CharSequence
-                    addSerializer(CharSequence::class.java, ToStringSerializer())
-                }
+            SimpleModule().apply {
+                // fallback for serializing CharSequence
+                addSerializer(CharSequence::class.java, ToStringSerializer())
+            }
         )
         private val channels by lazy { Channels(ChannelMongoDAO) }
     }
@@ -114,83 +114,86 @@ class WebConnector internal constructor(
             logger.debug("deploy web connector services for root path $path ")
 
             router.route(path)
-                    .handler(
-                            // "*"+credentials is rejected by browsers, so we use the equivalent regex instead
-                            CorsHandler.create(if (cookieAuth) ".*" else "*")
-                                    .allowedMethod(HttpMethod.POST)
-                                    .run {
-                                        if (sseEnabled) allowedMethod(HttpMethod.GET) else this
-                                    }
-                                    .allowedHeader("Access-Control-Allow-Origin")
-                                    .allowedHeader("Content-Type")
-                                    .allowedHeader("X-Requested-With").apply {
-                                        webConnectorExtraHeaders.forEach {
-                                            this.allowedHeader(it)
-                                        }
-                                    }
-                                    .allowCredentials(cookieAuth) // browsers do not send or save cookies unless credentials are allowed
-                    )
+                .handler(
+                    // "*"+credentials is rejected by browsers, so we use the equivalent regex instead
+                    CorsHandler.create(if (cookieAuth) ".*" else "*")
+                        .allowedMethod(HttpMethod.POST)
+                        .run {
+                            if (sseEnabled) allowedMethod(HttpMethod.GET) else this
+                        }
+                        .allowedHeader("Access-Control-Allow-Origin")
+                        .allowedHeader("Content-Type")
+                        .allowedHeader("X-Requested-With").apply {
+                            webConnectorExtraHeaders.forEach {
+                                this.allowedHeader(it)
+                            }
+                        }
+                        .allowCredentials(cookieAuth) // browsers do not send or save cookies unless credentials are allowed
+                )
             if (sseEnabled) {
                 router.route("$path/sse")
-                        .handler { context ->
-                            try {
-                                val userId = if (cookieAuth) {
-                                    getOrCreateUserIdCookie(context)
-                                } else {
-                                    context.queryParams()["userId"]
-                                }
-                                val response = context.response()
-                                response.isChunked = true
-                                response.headers().add("Content-Type", "text/event-stream;charset=UTF-8")
-                                response.headers().add("Connection", "keep-alive")
-                                response.headers().add("Cache-Control", "no-cache")
-                                response.sendSsePing()
-                                val timerId = vertx.setPeriodic(Duration.ofSeconds(sseKeepaliveDelay).toMillis()) {
-                                    response.sendSsePing()
-                                }
-                                val channelId = channels.register(applicationId, userId) { webConnectorResponse ->
-                                    response.write("event: message\n")
-                                    response.write("data: ${webMapper.writeValueAsString(webConnectorResponse)}\n\n")
-                                }
-                                response.closeHandler {
-                                    vertx.cancelTimer(timerId)
-                                    channels.unregister(channelId)
-                                }
-                            } catch (t: Throwable) {
-                                context.fail(t)
-                            }
-                        }
-            }
-            router.post(path)
                     .handler { context ->
                         try {
-                            executor.executeBlocking {
-                                val body = if (cookieAuth) {
-                                    val jsonBody = context.body().asJsonObject() ?: JsonObject()
-                                    val userId = getOrCreateUserIdCookie(context)
-                                    jsonBody.put("userId", userId)   // note: mutating jsonBody does not mutate the original buffer
-                                    jsonBody.toString()
-                                } else {
-                                    context.body().asString()
-                                }
-                                handleRequest(controller, context, body)
+                            val userId = if (cookieAuth) {
+                                getOrCreateUserIdCookie(context)
+                            } else {
+                                context.queryParams()["userId"]
                             }
-                        } catch (e: Throwable) {
-                            context.fail(e)
+                            val response = context.response()
+                            response.isChunked = true
+                            response.headers().add("Content-Type", "text/event-stream;charset=UTF-8")
+                            response.headers().add("Connection", "keep-alive")
+                            response.headers().add("Cache-Control", "no-cache")
+                            response.sendSsePing()
+                            val timerId = vertx.setPeriodic(Duration.ofSeconds(sseKeepaliveDelay).toMillis()) {
+                                response.sendSsePing()
+                            }
+                            val channelId = channels.register(applicationId, userId) { webConnectorResponse ->
+                                response.write("event: message\n")
+                                response.write("data: ${webMapper.writeValueAsString(webConnectorResponse)}\n\n")
+                            }
+                            response.closeHandler {
+                                vertx.cancelTimer(timerId)
+                                channels.unregister(channelId)
+                            }
+                        } catch (t: Throwable) {
+                            context.fail(t)
                         }
                     }
+            }
+            router.post(path)
+                .handler { context ->
+                    try {
+                        executor.executeBlocking {
+                            val body = if (cookieAuth) {
+                                val jsonBody = context.body().asJsonObject() ?: JsonObject()
+                                val userId = getOrCreateUserIdCookie(context)
+                                jsonBody.put(
+                                    "userId",
+                                    userId
+                                )   // note: mutating jsonBody does not mutate the original buffer
+                                jsonBody.toString()
+                            } else {
+                                context.body().asString()
+                            }
+                            handleRequest(controller, context, body)
+                        }
+                    } catch (e: Throwable) {
+                        context.fail(e)
+                    }
+                }
         }
     }
 
     private fun getOrCreateUserIdCookie(context: RoutingContext) = (context.request().getCookie(TOCK_USER_ID)?.value
-            ?: UUID.randomUUID().toString().also { uuid ->
-                context.response().addCookie(
-                        Cookie.cookie(TOCK_USER_ID, uuid).setHttpOnly(true).setSecure(true)
-                                .setSameSite(
-                                        CookieSameSite.NONE
-                                )
-                )
-            })
+        ?: UUID.randomUUID().toString().also { uuid ->
+            context.response().addCookie(
+                Cookie.cookie(TOCK_USER_ID, uuid).setHttpOnly(true).setSecure(true)
+                    .setSameSite(
+                        CookieSameSite.NONE
+                    )
+            )
+        })
 
     private fun HttpServerResponse.sendSsePing() {
         write("event: ping\n")
@@ -198,15 +201,15 @@ class WebConnector internal constructor(
     }
 
     override fun getOrchestrationHandlers(): OrchestrationHandlers =
-            OrchestrationHandlers(
-                    eligibilityHandler = this::handleEligibility,
-                    proxyHandler = this::handleProxy
-            )
+        OrchestrationHandlers(
+            eligibilityHandler = this::handleEligibility,
+            proxyHandler = this::handleProxy
+        )
 
     private fun handleRequest(
-            controller: ConnectorController,
-            context: RoutingContext,
-            body: String
+        controller: ConnectorController,
+        context: RoutingContext,
+        body: String
     ) {
         val timerData = BotRepository.requestTimer.start("web_webhook")
         try {
@@ -214,22 +217,22 @@ class WebConnector internal constructor(
             val request: WebConnectorRequest = mapper.readValue(body)
 
             val applicationId =
-                    if (request.connectorId?.isNotBlank() == true)
-                        if (webConnectorBridgeEnabled)
-                            request.connectorId.also { logger.debug { "Web bridge: $applicationId -> $it" } }
-                        else
-                            applicationId.also { logger.warn { "Web bridge disabled." } }
+                if (request.connectorId?.isNotBlank() == true)
+                    if (webConnectorBridgeEnabled)
+                        request.connectorId.also { logger.debug { "Web bridge: $applicationId -> $it" } }
                     else
-                        applicationId
+                        applicationId.also { logger.warn { "Web bridge disabled." } }
+                else
+                    applicationId
 
             val event = request.toEvent(applicationId)
             WebRequestInfosByEvent.put(event.id.toString(), WebRequestInfos(context.request()))
             val callback = WebConnectorCallback(
-                    applicationId = applicationId,
-                    locale = request.locale,
-                    context = context,
-                    webMapper = webMapper,
-                    eventId = event.id.toString()
+                applicationId = applicationId,
+                locale = request.locale,
+                context = context,
+                webMapper = webMapper,
+                eventId = event.id.toString()
             )
             controller.handle(event, ConnectorData(callback))
         } catch (t: Throwable) {
@@ -241,18 +244,18 @@ class WebConnector internal constructor(
     }
 
     private fun handleProxy(
-            controller: ConnectorController,
-            context: RoutingContext
+        controller: ConnectorController,
+        context: RoutingContext
     ) {
         val timerData = BotRepository.requestTimer.start("web_webhook_orchestred")
         try {
             logger.debug { "Web proxy request input : ${context.body().asString()}" }
             val request: ResumeOrchestrationRequest = mapper.readValue(context.body().asString())
             val callback = RestOrchestrationCallback(
-                    webConnectorType,
-                    applicationId = applicationId,
-                    context = context,
-                    orchestrationMapper = webMapper
+                webConnectorType,
+                applicationId = applicationId,
+                context = context,
+                orchestrationMapper = webMapper
             )
 
             controller.handle(request.toAction(), ConnectorData(callback))
@@ -265,28 +268,28 @@ class WebConnector internal constructor(
     }
 
     private fun handleEligibility(
-            controller: ConnectorController,
-            context: RoutingContext
+        controller: ConnectorController,
+        context: RoutingContext
     ) {
         val timerData = BotRepository.requestTimer.start("web_webhook_support")
         try {
             logger.debug { "Web support request input : ${context.body().asString()}" }
             val request: AskEligibilityToOrchestratedBotRequest = mapper.readValue(context.body().asString())
             val callback = RestOrchestrationCallback(
-                    webConnectorType,
-                    applicationId,
-                    context = context,
-                    orchestrationMapper = webMapper
+                webConnectorType,
+                applicationId,
+                context = context,
+                orchestrationMapper = webMapper
             )
 
             val support = controller.support(request.toAction(applicationId), ConnectorData(callback))
             val sendEligibility = SecondaryBotEligibilityResponse(
-                    support,
-                    OrchestrationMetaData(
-                            playerId = PlayerId(applicationId, bot),
-                            applicationId = applicationId,
-                            recipientId = request.metadata?.playerId ?: PlayerId(Dice.newId(), user)
-                    )
+                support,
+                OrchestrationMetaData(
+                    playerId = PlayerId(applicationId, bot),
+                    applicationId = applicationId,
+                    recipientId = request.metadata?.playerId ?: PlayerId(Dice.newId(), user)
+                )
             )
             callback.sendResponse(sendEligibility)
         } catch (t: Throwable) {
@@ -330,7 +333,7 @@ class WebConnector internal constructor(
         }
     }
 
-    override val persistProfileLoaded: Boolean = false
+    override val persistProfileLoaded: Boolean = booleanProperty("tock_web_connector_persist_profile", false)
 
     override fun loadProfile(callback: ConnectorCallback, userId: PlayerId): UserPreferences {
         return when (callback) {
@@ -340,11 +343,11 @@ class WebConnector internal constructor(
     }
 
     override fun addSuggestions(text: CharSequence, suggestions: List<CharSequence>): BotBus.() -> ConnectorMessage? =
-            { WebMessage(text.toString(), suggestions.map { webNlpQuickReply(it) }) }
+        { WebMessage(text.toString(), suggestions.map { webNlpQuickReply(it) }) }
 
     override fun addSuggestions(
-            message: ConnectorMessage,
-            suggestions: List<CharSequence>
+        message: ConnectorMessage,
+        suggestions: List<CharSequence>
     ): BotBus.() -> ConnectorMessage? = {
         (message as? WebMessage)?.let {
             if (it.card != null && it.card.buttons.isEmpty()) {
@@ -359,42 +362,42 @@ class WebConnector internal constructor(
 
     override fun toConnectorMessage(message: MediaMessage): BotBus.() -> List<ConnectorMessage> = {
         listOfNotNull(
-                when (message) {
-                    is MediaCard -> {
-                        WebMessage(
-                                card = WebCard(
-                                        title = message.title,
-                                        subTitle = message.subTitle,
-                                        file = message.file?.toWebMediaFile(),
-                                        buttons = message.actions.map { button -> button.toButton() }
-                                )
+            when (message) {
+                is MediaCard -> {
+                    WebMessage(
+                        card = WebCard(
+                            title = message.title,
+                            subTitle = message.subTitle,
+                            file = message.file?.toWebMediaFile(),
+                            buttons = message.actions.map { button -> button.toButton() }
                         )
-                    }
-
-                    is MediaCarousel -> {
-                        WebMessage(
-                                carousel = WebCarousel(
-                                        message.cards.map { mediaCard ->
-                                            WebCard(
-                                                    title = mediaCard.title,
-                                                    subTitle = mediaCard.subTitle,
-                                                    file = mediaCard.file?.toWebMediaFile(),
-                                                    buttons = mediaCard.actions.map { button -> button.toButton() }
-                                            )
-                                        }
-                                )
-                        )
-                    }
-
-                    else -> null
+                    )
                 }
+
+                is MediaCarousel -> {
+                    WebMessage(
+                        carousel = WebCarousel(
+                            message.cards.map { mediaCard ->
+                                WebCard(
+                                    title = mediaCard.title,
+                                    subTitle = mediaCard.subTitle,
+                                    file = mediaCard.file?.toWebMediaFile(),
+                                    buttons = mediaCard.actions.map { button -> button.toButton() }
+                                )
+                            }
+                        )
+                    )
+                }
+
+                else -> null
+            }
         )
     }
 
     private fun MediaAction.toButton() =
-            if (url == null) {
-                PostbackButton(title.toString(), null)
-            } else {
-                UrlButton(title.toString(), url.toString())
-            }
+        if (url == null) {
+            PostbackButton(title.toString(), null)
+        } else {
+            UrlButton(title.toString(), url.toString())
+        }
 }
