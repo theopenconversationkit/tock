bdefb4311c0bda773111feecc1499e95ae609b57
assouktim <32455560+assouktim@users.noreply.github.com>
2023-12-05 17:20:57 +0100

    [DERCBOT-753] Init LLM Orchestrator - maven module (python) (#276)

---
+++ llm/orchestrator-server/src/main/python/app/src/llm_orchestrator/routers/requests/requests.py
@@ -0,0 +1,46 @@
+#   Copyright (C) 2023 Credit Mutuel Arkea
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+from pydantic import BaseModel, Field
+
+from llm_orchestrator.models.em.em_types import EMSetting
+from llm_orchestrator.models.llm.llm_types import LLMSetting
+from llm_orchestrator.models.rag.rag_models import ChatMessage, MetadataFilter
+
+
+class RagQuery(BaseModel):
+    question: str = Field(
+        description='The user question.',
+        examples=['How to get started playing guitar ?'],
+    )
+    history: list[ChatMessage] = Field(
+        description="Conversation history, used to reformulate the user's question."
+    )
+    # condense_question_llm_setting: LLMSetting =
+    #   Field(description="LLM setting, used to condense the user's question.")
+    question_answering_llm_setting: LLMSetting = Field(
+        description='LLM setting, used to perform a QA Prompt.'
+    )
+    embedding_question_em_setting: EMSetting = Field(
+        description="Embedding model setting, used to calculate the user's question vector."
+    )
+    index_name: str = Field(
+        description='Index name corresponding to a document collection in the vector database.',
+        examples=['my-index-name'],
+    )
+    metadata_filters: list[MetadataFilter] = Field(
+        description='Metadata filters, used to search for specific documents. The AND operator is applied, '
+        'so all filters should be satisfied.'
+    )
---
+++ llm/orchestrator-server/src/main/python/app/src/llm_orchestrator/services/langchain/factories/langchain_factory.py
@@ -0,0 +1,66 @@
+#   Copyright (C) 2023 Credit Mutuel Arkea
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+from llm_orchestrator.exceptions.error_code import ErrorCode
+from llm_orchestrator.exceptions.functional_exception import (
+    FunctionalException,
+)
+from llm_orchestrator.models.em.azureopenai.azure_openai_em_setting import (
+    AzureOpenAIEMSetting,
+)
+from llm_orchestrator.models.em.em_setting import BaseEMSetting
+from llm_orchestrator.models.em.openai.openai_em_setting import OpenAIEMSetting
+from llm_orchestrator.models.llm.azureopenai.azure_openai_llm_setting import (
+    AzureOpenAILLMSetting,
+)
+from llm_orchestrator.models.llm.llm_setting import BaseLLMSetting
+from llm_orchestrator.models.llm.openai.openai_llm_setting import (
+    OpenAILLMSetting,
+)
+from llm_orchestrator.services.langchain.factories.em.azure_openai_em_factory import (
+    AzureOpenAIEMFactory,
+)
+from llm_orchestrator.services.langchain.factories.em.em_factory import (
+    LangChainEMFactory,
+)
+from llm_orchestrator.services.langchain.factories.em.openai_em_factory import (
+    OpenAIEMFactory,
+)
+from llm_orchestrator.services.langchain.factories.llm.azure_openai_llm_factory import (
+    AzureOpenAILLMFactory,
+)
+from llm_orchestrator.services.langchain.factories.llm.llm_factory import (
+    LangChainLLMFactory,
+)
+from llm_orchestrator.services.langchain.factories.llm.openai_llm_factory import (
+    OpenAILLMFactory,
+)
+
+
+def get_llm_factory(setting: BaseLLMSetting) -> LangChainLLMFactory:
+    if isinstance(setting, OpenAILLMSetting):
+        return OpenAILLMFactory(setting=setting)
+    elif isinstance(setting, AzureOpenAILLMSetting):
+        return AzureOpenAILLMFactory(setting=setting)
+    else:
+        raise FunctionalException(ErrorCode.E10)
+
+
+def get_em_factory(setting: BaseEMSetting) -> LangChainEMFactory:
+    if isinstance(setting, OpenAIEMSetting):
+        return OpenAIEMFactory(setting=setting)
+    elif isinstance(setting, AzureOpenAIEMSetting):
+        return AzureOpenAIEMFactory(setting=setting)
+    else:
+        raise FunctionalException(ErrorCode.E10)
