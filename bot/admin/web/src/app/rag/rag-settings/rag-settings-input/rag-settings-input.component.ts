7f2ff29db248fe4683e120d563e113224614cf29
rkuffer <101799433+rkuffer@users.noreply.github.com>
2024-02-01 15:20:03 +0100

    Obfuscation of inputs (#296)

--- bot/admin/web/src/app/rag/rag-settings/rag-settings-input/rag-settings-input.component.ts
+++ bot/admin/web/src/app/rag/rag-settings/rag-settings-input/rag-settings-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { DefaultPrompt, EnginesConfigurationParam } from '../models/engines-configurations';
 import { FormControl, FormGroup } from '@angular/forms';
 
@@ -7,15 +7,15 @@ import { FormControl, FormGroup } from '@angular/forms';
   templateUrl: './rag-settings-input.component.html',
   styleUrls: ['./rag-settings-input.component.scss']
 })
-export class RagSettingsInputComponent implements OnInit {
+export class RagSettingsInputComponent {
   @Input() configurationParam: EnginesConfigurationParam;
   @Input() parentGroup: string;
-  @Input() isSubmitted: boolean;
   @Input() form: FormGroup;
+  @Input() isSubmitted: boolean;
 
-  constructor() {}
+  @ViewChild('clearInput') clearInput: ElementRef;
 
-  ngOnInit(): void {}
+  inputVisible: boolean = false;
 
   getFormControl(): FormControl {
     return this.form.get(this.parentGroup).get(this.configurationParam.key) as FormControl;
@@ -24,5 +24,54 @@ export class RagSettingsInputComponent implements OnInit {
   restoreDefaultPrompt(): void {
     this.form.get(this.parentGroup).get('prompt').setValue(DefaultPrompt);
     this.form.get(this.parentGroup).get('prompt').markAsDirty();
+  }
+
+  showInput(event: FocusEvent): void {
+    const target = event.target as HTMLInputElement;
+
+    setTimeout(() => {
+      const selectionStart = target.selectionStart;
+      this.inputVisible = true;
+      setTimeout(() => {
+        const clearInputElem = this.clearInput.nativeElement;
+        clearInputElem.focus();
+        if (Number.isInteger(selectionStart)) clearInputElem.setSelectionRange(selectionStart, selectionStart);
+      });
+    });
+  }
+
+  hideInput(): void {
+    this.inputVisible = false;
+  }
+
+  getControlObfuscatedValue(): string {
+    let str = this.getFormControl().value;
+    if (!str) return '';
+
+    let url: URL;
+    let strLen: number;
+    let protocol: string;
+
+    try {
+      url = new URL(str);
+    } catch (_) {}
+
+    if (url) {
+      protocol = url.protocol;
+      str = str.replace(protocol, '');
+      strLen = str.length;
+    } else {
+      strLen = str.length;
+    }
+
+    const nbVisibleChars = Math.min(3, Math.floor(strLen / 8.1));
+    if (nbVisibleChars) {
+      const nbHiddens = strLen - 2 * nbVisibleChars;
+      const obfuscation = [str.substring(0, nbVisibleChars), '*'.repeat(nbHiddens), str.slice(nbVisibleChars * -1)].join('');
+
+      return [protocol, obfuscation].join('');
+    }
+
+    return '*'.repeat(strLen);
   }
 }
