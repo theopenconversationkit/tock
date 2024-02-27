5e863085aa383625ac74f05f5377359b488c8edf
Diverrez morgan <97022189+morgandiverrez@users.noreply.github.com>
2024-02-27 12:07:15 +0100

    [DERCBOT-778] Sentence Generation Feature Configuration. (#311)

---
+++ bot/admin/server/src/main/kotlin/model/BotGenerateSentencesConfigurationDTO.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2017/2021 e-voyageurs technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.tock.bot.admin.model
+
+import ai.tock.bot.admin.bot.generatesentences.BotGenerateSentencesConfiguration
+import ai.tock.genai.orchestratorcore.models.llm.LLMSetting
+import org.litote.kmongo.newId
+import org.litote.kmongo.toId
+
+data class BotGenerateSentencesConfigurationDTO(
+    val id: String? = null,
+    val namespace: String,
+    val botId: String,
+    val enabled: Boolean = false,
+    val llmSetting: LLMSetting,
+) {
+    constructor(configuration: BotGenerateSentencesConfiguration): this(
+        configuration._id.toString(),
+        configuration.namespace,
+        configuration.botId,
+        configuration.enabled,
+        configuration.llmSetting,
+    )
+    fun toGenerateSentencesConfiguration(): BotGenerateSentencesConfiguration =
+        BotGenerateSentencesConfiguration(
+            id?.toId() ?: newId(),
+            namespace,
+            botId,
+            enabled,
+            llmSetting
+        )
+}
+
+
+
