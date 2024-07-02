df1cc3026dd76f7bc3d623ef3f0c0388aba885c5
Pierre Therrode <PTherrode@oui.sncf>
2024-07-02 18:11:51 +0200

    [WIP] Api Messenger update

--- bot/connector-messenger/src/main/kotlin/MessengerClient.kt
+++ bot/connector-messenger/src/main/kotlin/MessengerClient.kt
@@ -59,7 +59,7 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-private const val VERSION = "5.0"
+private const val VERSION = "20.0"
 
 /**
  * Messenger client.
