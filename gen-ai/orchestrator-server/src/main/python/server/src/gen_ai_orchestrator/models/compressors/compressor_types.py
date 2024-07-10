3165e200d0ee5adcc6f75f1f2fc9c9aededb808e
Morgan Diverrez <morgan.diverrez@arkea.com>
2024-07-10 15:15:52 +0200

    model compressor provider and params with flashrank_rerank specific one

---
+++ gen-ai/orchestrator-server/src/main/python/server/src/gen_ai_orchestrator/models/compressors/compressor_types.py
@@ -0,0 +1,28 @@
+#   Copyright (C) 2023-2024 Credit Mutuel Arkea
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+"""Module defining generic type alias"""
+
+from typing import Annotated, Union
+
+from fastapi import Body
+
+from gen_ai_orchestrator.models.compressors.flashrank_rerank.flashrank_rerank_params import (
+    FlashrankRerankCompressorParams,
+)
+
+DocumentCompressorParams = Annotated[
+    Union[FlashrankRerankCompressorParams],
+    Body(discriminator='provider'),
+]
---
+++ gen-ai/orchestrator-server/src/main/python/server/src/gen_ai_orchestrator/models/compressors/flashrank_rerank/flashrank_rerank_params.py
@@ -0,0 +1,41 @@
+#   Copyright (C) 2023-2024 Credit Mutuel Arkea
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+"""Model for creating FlashrankRerankCompressorSetting."""
+
+from typing import Literal, Optional
+
+from pydantic import Field
+
+from gen_ai_orchestrator.models.compressors.compressor_provider import (
+    CompressorProvider,
+)
+from gen_ai_orchestrator.models.compressors.document_compressor_params import (
+    BaseCompressorParams,
+)
+
+
+class FlashrankRerankCompressorParams(BaseCompressorParams):
+    provider: Literal[CompressorProvider.FLASHRANK_RERANK] = Field(
+        description='The Flashrank Rerank Model Provider.',
+        examples=[CompressorProvider.FLASHRANK_RERANK],
+    )
+    model: Optional[str] = Field(
+        description='The model id', examples=['text-embedding-ada-002'], default=None
+    )
+    min_score: Optional[float] = Field(description='Minimum retailment score.')
+    max_documents: Optional[int] = Field(
+        description='Maximum number of documents to return',
+        default=4,
+    )
