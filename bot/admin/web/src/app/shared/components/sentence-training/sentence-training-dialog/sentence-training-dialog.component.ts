8a3cc13fa1b5c493a5689d50e92380e5f3ae4756
Rodolphe Küffer <101799433+rkuffer@users.noreply.github.com>
2024-10-09 10:11:45 +0200

    Create a faq from rag dialog from inbox dialog display (#1752)

--- bot/admin/web/src/app/shared/components/sentence-training/sentence-training-dialog/sentence-training-dialog.component.ts
+++ bot/admin/web/src/app/shared/components/sentence-training/sentence-training-dialog/sentence-training-dialog.component.ts
@@ -5,9 +5,10 @@ import { take } from 'rxjs/operators';
 import { AnalyticsService } from '../../../../analytics/analytics.service';
 import { DialogReportQuery } from '../../../../analytics/dialogs/dialogs';
 import { StateService } from '../../../../core-nlp/state.service';
-import { ActionReport, DialogReport, Sentence } from '../../../../shared/model/dialog-data';
+import { ActionReport, Debug, DialogReport, Sentence, SentenceWithFootnotes } from '../../../../shared/model/dialog-data';
 import { SentenceExtended } from '../sentence-training.component';
 import { getDialogMessageUserAvatar, getDialogMessageUserQualifier } from '../../../utils';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'tock-sentence-training-dialog',
@@ -25,7 +26,8 @@ export class SentenceTrainingDialogComponent implements OnChanges, OnDestroy {
     private state: StateService,
     private readonly analyticsService: AnalyticsService,
     private readonly elementRef: ElementRef,
-    private cd: ChangeDetectorRef
+    private cd: ChangeDetectorRef,
+    private router: Router
   ) {}
 
   dialogs: DialogReport[] = [];
@@ -109,6 +111,29 @@ export class SentenceTrainingDialogComponent implements OnChanges, OnDestroy {
     if (action.isBot()) return;
     if (action.message.isSentence()) {
       this.onSearchSentence.emit(action.message);
+    }
+  }
+
+  createFaq(action: ActionReport, actionsStack: ActionReport[]) {
+    const actionIndex = actionsStack.findIndex((act) => act === action);
+    if (actionIndex > 0) {
+      const answerSentence = action.message as unknown as SentenceWithFootnotes;
+      const answer = answerSentence.text;
+
+      let question;
+      const questionAction = actionsStack[actionIndex - 1];
+
+      if (questionAction.message.isDebug()) {
+        const actionDebug = questionAction.message as unknown as Debug;
+        question = actionDebug.data.condense_question || actionDebug.data.user_question;
+      } else if (!questionAction.isBot()) {
+        const questionSentence = questionAction.message as unknown as Sentence;
+        question = questionSentence.text;
+      }
+
+      if (question && answer) {
+        this.router.navigate(['faq/management'], { state: { question, answer } });
+      }
     }
   }
 
