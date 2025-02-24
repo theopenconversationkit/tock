b1e3a8d3c69642d5c1a8ea3a3808d405935720aa
Rodolphe Küffer <101799433+rkuffer@users.noreply.github.com>
2025-02-24 10:54:13 +0100

    [Tock Studio] Refacto of Gen AI prompt settings (#1833)
    * Wip
    
    * Wip
    
    * Wip
    
    * Gen AI prompt settings refacto
    
    * Misc
    
    * Misc improvements

--- bot/admin/web/src/app/configuration/vector-db-settings/models/providers-configuration.ts
+++ bot/admin/web/src/app/configuration/vector-db-settings/models/providers-configuration.ts
@@ -19,18 +19,7 @@ export const ProvidersConfigurations: VectorDbProvidersConfiguration[] = [
       { key: 'host', label: 'Host', type: 'text', defaultValue: 'localhost' },
       { key: 'port', label: 'Port', type: 'number', numberControlType: 'input', min: 1, max: 65535, step: 1, defaultValue: '9200' },
       { key: 'username', label: 'User name', type: 'obfuscated', defaultValue: 'admin' },
-      { key: 'password', label: 'Password', type: 'obfuscated', defaultValue: 'admin', confirmExport: true },
-      {
-        key: 'k',
-        label: 'k-nearest neighbors',
-        type: 'number',
-        numberControlType: 'input',
-        min: 1,
-        max: 100,
-        step: 1,
-        defaultValue: 4,
-        information: 'Maximum number of nearest neighbors to return in search queries'
-      }
+      { key: 'password', label: 'Password', type: 'obfuscated', defaultValue: 'admin', confirmExport: true }
     ]
   },
   {
@@ -41,17 +30,6 @@ export const ProvidersConfigurations: VectorDbProvidersConfiguration[] = [
       { key: 'port', label: 'Port', type: 'number', numberControlType: 'input', min: 1, max: 65535, step: 1, defaultValue: '5432' },
       { key: 'username', label: 'User name', type: 'obfuscated', defaultValue: 'postgres' },
       { key: 'password', label: 'Password', type: 'obfuscated', defaultValue: 'ChangeMe', confirmExport: true },
-      {
-        key: 'k',
-        label: 'k-nearest neighbors',
-        type: 'number',
-        numberControlType: 'input',
-        min: 1,
-        max: 100,
-        step: 1,
-        defaultValue: 4,
-        information: 'Maximum number of nearest neighbors to return in search queries'
-      },
       {
         key: 'database',
         label: 'Database',
--- bot/admin/web/src/app/rag/rag-settings/models/engines-configurations.ts
+++ bot/admin/web/src/app/rag/rag-settings/models/engines-configurations.ts
@@ -6,10 +6,14 @@ import {
   OllamaEmModelsList,
   OllamaLlmModelsList,
   OpenAIEmbeddingModel,
-  OpenAIModelsList
+  OpenAIModelsList,
+  ProvidersConfigurationParam,
+  PromptDefinitionFormatter
 } from '../../../shared/model/ai-settings';
 
-export const DefaultPrompt: string = `# TOCK (The Open Conversation Kit) chatbot
+export const QuestionCondensingDefaultPrompt: string = `Given a chat history and the latest user question which might reference context in the chat history, formulate a standalone question which can be nderstood without the chat history. Do NOT answer the question, just reformulate it if needed and otherwise return it as is.`;
+
+export const QuestionAnsweringDefaultPrompt: string = `# TOCK (The Open Conversation Kit) chatbot
 
 ## General context
 
@@ -26,17 +30,55 @@ Your tone is empathetic, informative and polite.
 ## Additional instructions
 
 Use the following pieces of retrieved context to answer the question.
-If you dont know the answer, answer (exactly) with "{no_answer}".
-Answer in {locale}.
+If you dont know the answer, answer (exactly) with "{{no_answer}}".
+Answer in {{locale}}.
 
 ## Context
 
-{context}
+{{context}}
 
 ## Question
 
-{question}
+{{question}}
 `;
+
+export const QuestionCondensing_prompt: ProvidersConfigurationParam[] = [
+  {
+    key: 'formatter',
+    label: 'Prompt template format',
+    type: 'radio',
+    source: [PromptDefinitionFormatter.jinja2, PromptDefinitionFormatter.fstring],
+    defaultValue: PromptDefinitionFormatter.jinja2,
+    inputScale: 'fullwidth'
+  },
+  {
+    key: 'template',
+    label: 'Prompt template',
+    type: 'prompt',
+    inputScale: 'fullwidth',
+    defaultValue: QuestionCondensingDefaultPrompt,
+    information: "See LangChain's ChatPromptTemplate for more infos"
+  }
+];
+
+export const QuestionAnswering_prompt: ProvidersConfigurationParam[] = [
+  {
+    key: 'formatter',
+    label: 'Prompt template format',
+    type: 'radio',
+    source: [PromptDefinitionFormatter.jinja2, PromptDefinitionFormatter.fstring],
+    defaultValue: PromptDefinitionFormatter.jinja2,
+    inputScale: 'fullwidth'
+  },
+  {
+    key: 'template',
+    label: 'Prompt template',
+    type: 'prompt',
+    inputScale: 'fullwidth',
+    defaultValue: QuestionAnsweringDefaultPrompt,
+    rows: 16
+  }
+];
 
 const EnginesConfigurations_Llm: EnginesConfiguration[] = [
   {
@@ -46,8 +88,7 @@ const EnginesConfigurations_Llm: EnginesConfiguration[] = [
       { key: 'apiKey', label: 'Api key', type: 'obfuscated', confirmExport: true },
       { key: 'baseUrl', label: 'Base url', type: 'text', defaultValue: 'https://api.openai.com/v1' },
       { key: 'model', label: 'Model name', type: 'openlist', source: OpenAIModelsList },
-      { key: 'temperature', label: 'Temperature', type: 'number', inputScale: 'fullwidth', min: 0, max: 1, step: 0.05 },
-      { key: 'prompt', label: 'Prompt', type: 'prompt', inputScale: 'fullwidth', defaultValue: DefaultPrompt }
+      { key: 'temperature', label: 'Temperature', type: 'number', inputScale: 'fullwidth', min: 0, max: 1, step: 0.05 }
     ]
   },
   {
@@ -59,8 +100,7 @@ const EnginesConfigurations_Llm: EnginesConfiguration[] = [
       { key: 'deploymentName', label: 'Deployment name', type: 'text' },
       { key: 'model', label: 'Model name', type: 'openlist', source: OpenAIModelsList },
       { key: 'apiBase', label: 'Base url', type: 'obfuscated' },
-      { key: 'temperature', label: 'Temperature', type: 'number', inputScale: 'fullwidth', min: 0, max: 1, step: 0.05 },
-      { key: 'prompt', label: 'Prompt', type: 'prompt', inputScale: 'fullwidth', defaultValue: DefaultPrompt }
+      { key: 'temperature', label: 'Temperature', type: 'number', inputScale: 'fullwidth', min: 0, max: 1, step: 0.05 }
     ]
   },
   {
@@ -69,8 +109,7 @@ const EnginesConfigurations_Llm: EnginesConfiguration[] = [
     params: [
       { key: 'baseUrl', label: 'BaseUrl', type: 'text', defaultValue: 'http://localhost:11434' },
       { key: 'model', label: 'Model', type: 'openlist', source: OllamaLlmModelsList, defaultValue: 'llama2' },
-      { key: 'temperature', label: 'Temperature', type: 'number', inputScale: 'fullwidth', min: 0, max: 1, step: 0.05, defaultValue: 0.7 },
-      { key: 'prompt', label: 'Prompt', type: 'prompt', inputScale: 'fullwidth', defaultValue: DefaultPrompt }
+      { key: 'temperature', label: 'Temperature', type: 'number', inputScale: 'fullwidth', min: 0, max: 1, step: 0.05, defaultValue: 0.7 }
     ]
   }
 ];
@@ -106,7 +145,15 @@ const EnginesConfigurations_Embedding: EnginesConfiguration[] = [
   }
 ];
 
-export const EnginesConfigurations: { [K in AiEngineSettingKeyName]: EnginesConfiguration[] } = {
-  llmSetting: EnginesConfigurations_Llm,
+export const EnginesConfigurations: {
+  [K in Extract<
+    AiEngineSettingKeyName,
+    | AiEngineSettingKeyName.questionAnsweringLlmSetting
+    | AiEngineSettingKeyName.questionCondensingLlmSetting
+    | AiEngineSettingKeyName.emSetting
+  >]: EnginesConfiguration[];
+} = {
+  questionCondensingLlmSetting: EnginesConfigurations_Llm,
+  questionAnsweringLlmSetting: EnginesConfigurations_Llm,
   emSetting: EnginesConfigurations_Embedding
 };
