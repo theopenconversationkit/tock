b65a7730d8f077e1a6ad690bd26ee74bf4c29427
Benjamin BERNARD <benjamin.bernard@arkea.com>
2025-12-09 15:51:40 +0100

    Atelier feedback.

--- bot/connector-web/src/main/kotlin/WebMessage.kt
+++ bot/connector-web/src/main/kotlin/WebMessage.kt
@@ -26,6 +26,7 @@ import ai.tock.bot.connector.web.send.WebImage
 import ai.tock.bot.connector.web.send.WebMessageContent
 import ai.tock.bot.connector.web.send.WebMessageContract
 import ai.tock.bot.connector.web.send.WebWidget
+import ai.tock.bot.engine.action.Action
 import ai.tock.bot.engine.message.GenericMessage
 import ai.tock.shared.mapNotNullValues
 import com.fasterxml.jackson.annotation.JsonIgnore
@@ -33,6 +34,7 @@ import com.fasterxml.jackson.annotation.JsonInclude
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 data class WebMessage(
+    override val actionId: String,
     override val text: String? = null,
     override val buttons: List<Button> = emptyList(),
     override val card: WebCard? = null,
@@ -44,6 +46,7 @@ data class WebMessage(
     override val footnotes: List<Footnote> = emptyList(),
 ) : WebMessageContract, WebConnectorMessage {
     constructor(content: WebMessageContent) : this(
+        content.actionId,
         content.text,
         content.buttons,
         content.card,
