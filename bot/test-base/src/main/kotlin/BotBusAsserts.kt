33545711e7d0c258bfc7b014fbba520dd3f27b63
Fabilin <fabilin.dev@pm.me>
2025-12-26 12:20:28 +0100

    resolves #1970 [Test Toolkit] fix deprecations

--- bot/test-base/src/main/kotlin/BotBusAsserts.kt
+++ bot/test-base/src/main/kotlin/BotBusAsserts.kt
@@ -25,180 +25,171 @@ import ai.tock.bot.engine.message.Attachment
 import ai.tock.bot.engine.message.Choice
 import ai.tock.bot.engine.message.GenericElement
 import ai.tock.bot.engine.message.GenericMessage
-import ch.tutteli.atrium.api.cc.en_GB.any
-import ch.tutteli.atrium.api.cc.en_GB.contains
-import ch.tutteli.atrium.api.cc.en_GB.containsExactly
-import ch.tutteli.atrium.api.cc.en_GB.containsNot
-import ch.tutteli.atrium.api.cc.en_GB.notToBeNull
-import ch.tutteli.atrium.api.cc.en_GB.property
-import ch.tutteli.atrium.api.cc.en_GB.returnValueOf
-import ch.tutteli.atrium.api.cc.en_GB.toBe
-import ch.tutteli.atrium.creating.Assert
-import ch.tutteli.atrium.domain.builders.AssertImpl
+import ch.tutteli.atrium.api.fluent.en_GB.any
+import ch.tutteli.atrium.api.fluent.en_GB.contains
+import ch.tutteli.atrium.api.fluent.en_GB.containsExactly
+import ch.tutteli.atrium.api.fluent.en_GB.containsNot
+import ch.tutteli.atrium.api.fluent.en_GB.notToBeNull
+import ch.tutteli.atrium.api.fluent.en_GB.feature
+import ch.tutteli.atrium.api.fluent.en_GB.feature
+import ch.tutteli.atrium.api.fluent.en_GB.toBe
+import ch.tutteli.atrium.creating.Expect
+import ch.tutteli.atrium.domain.builders.ExpectImpl
 import ch.tutteli.atrium.domain.creating.any.typetransformation.AnyTypeTransformation
 import ch.tutteli.atrium.reporting.RawString
 import ch.tutteli.atrium.reporting.translating.Untranslatable
-import ch.tutteli.atrium.verbs.expect
+import ch.tutteli.atrium.api.verbs.expect
+import ch.tutteli.atrium.assertions.builders.assertionBuilder
+import ch.tutteli.atrium.core.Option
+import ch.tutteli.atrium.core.Some
+import ch.tutteli.atrium.logic._logic
+import ch.tutteli.atrium.logic.changeSubject
+import ch.tutteli.atrium.logic.creating.transformers.impl.subjectchanger.DefaultFailureHandlerImpl
+import ch.tutteli.atrium.logic.creating.transformers.subjectChanger
+import ch.tutteli.atrium.reporting.Text
+import kotlin.collections.map
 
-fun Assert<BotBusMockLog>.toBeSimpleTextMessage(expectedText: String) = returnValueOf(BotBusMockLog::text).toBe(expectedText)
+fun Expect<BotBusMockLog>.toBeSimpleTextMessage(expectedText: String) = feature(BotBusMockLog::text).toBe(expectedText)
 
-fun Assert<BotBusMockLog>.asGenericMessage(assertionCreator: Assert<GenericMessage>.() -> Unit) {
-    val parameterObject =
-        AnyTypeTransformation.ParameterObject(
-            Untranslatable("is a"),
-            RawString.create(GenericMessage::class.simpleName!!),
-            this,
-            assertionCreator,
-            Untranslatable("cannot be converted to generic message : is not a send sentence"),
-        )
-    AssertImpl.any.typeTransformation.transform(
-        parameterObject,
-        { it.action is SendSentence && it.genericMessage() != null },
-        { it.genericMessage()!! },
-        AssertImpl.any.typeTransformation.failureHandlers.newExplanatory(),
+fun Expect<BotBusMockLog>.asGenericMessage(assertionCreator: Expect<GenericMessage>.() -> Unit) {
+    _logic.subjectChanger.reported(
+        _logic,
+        Untranslatable("is a"),
+        Text(GenericMessage::class.simpleName!!),
+        { Option.someIf(
+            it.action is SendSentence && it.genericMessage() != null,
+            { it.genericMessage()!! }
+        ) },
+        DefaultFailureHandlerImpl(),
+        Some(assertionCreator),
     )
 }
 
-fun Assert<GenericMessage>.toHaveGlobalText(
+fun Expect<GenericMessage>.toHaveGlobalText(
     expectedText: String,
     textName: String = "text",
-) = property(GenericMessage::texts).addAssertionsCreatedBy {
-    returnValueOf(Map<String, String>::get, textName).toBe(expectedText)
+) = feature(GenericMessage::texts) {
+    feature(Map<String, String>::get, textName).toBe(expectedText)
 }
 
-fun Assert<GenericMessage>.toHaveGlobalChoices(
+fun Expect<GenericMessage>.toHaveGlobalChoices(
     expectedChoice: String,
     vararg otherExpectedChoices: String,
-) = property(GenericMessage::choices).addAssertionsCreatedBy {
-    expect(
-        subject.map { choice ->
+): Expect<List<Choice>> = feature(GenericMessage::choices).feature("title", {
+        map { choice ->
             choice.parameters[SendChoice.TITLE_PARAMETER]
-        },
-    ).contains(expectedChoice, *otherExpectedChoices)
-}
+        }
+    }, { contains(expectedChoice, *otherExpectedChoices) })
 
-fun Assert<GenericMessage>.toHaveNotGlobalChoices(
+fun Expect<GenericMessage>.toHaveNotGlobalChoices(
     unexpectedChoice: String,
     vararg otherUnexpectedChoices: String,
-) = property(GenericMessage::choices).addAssertionsCreatedBy {
-    expect(
-        subject.map { choice ->
-            choice.parameters[SendChoice.TITLE_PARAMETER]
-        },
-    ).containsNot(unexpectedChoice, *otherUnexpectedChoices)
-}
+): Expect<List<Choice>> = feature(GenericMessage::choices).feature("title", {
+    map { choice ->
+        choice.parameters[SendChoice.TITLE_PARAMETER]
+    }
+}, { containsNot(unexpectedChoice, *otherUnexpectedChoices) })
 
-fun Assert<GenericMessage>.toHaveExactlyGlobalChoices(
+fun Expect<GenericMessage>.toHaveExactlyGlobalChoices(
     expectedChoice: String,
     vararg otherExpectedChoices: String,
-) = property(GenericMessage::choices).addAssertionsCreatedBy {
-    expect(
-        subject.map { choice ->
-            choice.parameters[SendChoice.TITLE_PARAMETER]
-        },
-    ).containsExactly(expectedChoice, *otherExpectedChoices)
-}
-
-fun Assert<GenericMessage>.toHaveElement(
-    index: Int,
-    assertionCreator: Assert<GenericElement>.() -> Unit,
-) = property(GenericMessage::subElements).addAssertionsCreatedBy {
-    returnValueOf(List<GenericElement>::get, index) {
-        addAssertionsCreatedBy(assertionCreator)
+): Expect<List<Choice>> = feature(GenericMessage::choices).feature("title", {
+    map { choice ->
+        choice.parameters[SendChoice.TITLE_PARAMETER]
     }
+}, { containsExactly(expectedChoice, *otherExpectedChoices) })
+
+fun Expect<GenericMessage>.toHaveElement(
+    index: Int,
+    assertionCreator: Expect<GenericElement>.() -> Unit,
+): Expect<GenericMessage> = feature(GenericMessage::subElements) {
+    feature(List<GenericElement>::get, index, assertionCreator)
 }
 
-fun Assert<GenericElement>.toHaveText(
+fun Expect<GenericElement>.toHaveText(
     expectedText: String,
     textName: String,
-) = property(GenericElement::texts).addAssertionsCreatedBy {
-    returnValueOf(Map<String, String>::get, textName).toBe(expectedText)
+): Expect<GenericElement> = feature(GenericElement::texts) {
+    feature(Map<String, String>::get, textName).toBe(expectedText)
 }
 
-fun Assert<GenericElement>.toHaveTitle(expectedTitle: String) = toHaveText(expectedTitle, "title")
+fun Expect<GenericElement>.toHaveTitle(expectedTitle: String): Expect<GenericElement> = toHaveText(expectedTitle, "title")
 
-fun Assert<GenericElement>.toHaveSubtitle(expectedSubtitle: String) = toHaveText(expectedSubtitle, "subtitle")
+fun Expect<GenericElement>.toHaveSubtitle(expectedSubtitle: String): Expect<GenericElement> = toHaveText(expectedSubtitle, "subtitle")
 
-fun Assert<GenericElement>.toHaveChoices(
+fun Expect<GenericElement>.toHaveChoices(
     expectedChoice: String,
     vararg otherExpectedChoices: String,
-) = property(GenericElement::choices).addAssertionsCreatedBy {
-    expect(
-        subject.mapNotNull { choice ->
-            choice.parameters[SendChoice.TITLE_PARAMETER]
-        },
-    ).contains(expectedChoice, *otherExpectedChoices)
-}
+): Expect<List<Choice>> = feature(GenericElement::choices).feature("title", {
+    mapNotNull { choice ->
+        choice.parameters[SendChoice.TITLE_PARAMETER]
+    }
+}, { contains(expectedChoice, *otherExpectedChoices) })
 
-fun Assert<GenericElement>.toHaveNotChoices(
+fun Expect<GenericElement>.toHaveNotChoices(
     unexpectedChoice: String,
     vararg otherUnexpectedChoices: String,
-) = property(GenericElement::choices).addAssertionsCreatedBy {
-    expect(
-        subject.mapNotNull { choice ->
-            choice.parameters[SendChoice.TITLE_PARAMETER]
-        },
-    ).containsNot(unexpectedChoice, *otherUnexpectedChoices)
-}
+): Expect<List<Choice>> = feature(GenericElement::choices).feature("title", {
+    mapNotNull { choice ->
+        choice.parameters[SendChoice.TITLE_PARAMETER]
+    }
+}, { containsNot(unexpectedChoice, *otherUnexpectedChoices) })
 
-fun Assert<GenericElement>.toHaveExactlyChoices(
+fun Expect<GenericElement>.toHaveExactlyChoices(
     expectedChoice: String,
     vararg otherExpectedChoices: String,
-) = property(GenericElement::choices).addAssertionsCreatedBy {
-    expect(
-        subject.mapNotNull { choice ->
-            choice.parameters[SendChoice.TITLE_PARAMETER]
-        },
-    ).containsExactly(expectedChoice, *otherExpectedChoices)
-}
+): Expect<List<Choice>> = feature(GenericElement::choices).feature("title", {
+    mapNotNull { choice ->
+        choice.parameters[SendChoice.TITLE_PARAMETER]
+    }
+}, { containsExactly(expectedChoice, *otherExpectedChoices) })
 
-fun ConnectorMessage.asGenericMessage(assertionCreator: Assert<GenericMessage>.() -> Unit) {
+fun ConnectorMessage.asGenericMessage(assertionCreator: Expect<GenericMessage>.() -> Unit) {
     expect(toGenericMessage()).notToBeNull(assertionCreator)
 }
 
-fun Assert<GenericElement>.toHaveAttachment(assertionCreator: Assert<Attachment>.() -> Unit) =
-    property(GenericElement::attachments).addAssertionsCreatedBy {
-        any {
-            addAssertionsCreatedBy(assertionCreator)
-        }
-    }
-
-fun Assert<Attachment>.toHaveUrl(url: String) = property(Attachment::url).toBe(url)
-
-fun Assert<Attachment>.toBeImage() = property(Attachment::type).toBe(SendAttachment.AttachmentType.image)
-
-fun Assert<GenericMessage>.toHaveChoice(
-    title: String,
-    assertionCreator: Assert<Choice>.() -> Unit,
-) = property(GenericMessage::choices).addAssertionsCreatedBy {
-    any {
-        toHaveTitle(title)
-        addAssertionsCreatedBy(assertionCreator)
+fun Expect<GenericElement>.toHaveAttachment(assertionCreator: Expect<Attachment>.() -> Unit) {
+    feature(GenericElement::attachments) {
+        any(assertionCreator)
     }
 }
 
-fun Assert<Choice>.toHaveTitle(title: String) {
+fun Expect<Attachment>.toHaveUrl(url: String) = feature(Attachment::url).toBe(url)
+
+fun Expect<Attachment>.toBeImage() = feature(Attachment::type).toBe(SendAttachment.AttachmentType.image)
+
+fun Expect<GenericMessage>.toHaveChoice(
+    title: String,
+    assertionCreator: Expect<Choice>.() -> Unit,
+): Expect<GenericMessage> = feature(GenericMessage::choices) {
+    any {
+        toHaveTitle(title)
+        assertionCreator()
+    }
+}
+
+fun Expect<Choice>.toHaveTitle(title: String) {
     toHaveParameter(SendChoice.TITLE_PARAMETER, title)
 }
 
-fun Assert<Choice>.toHaveIntent(intentName: String) {
-    property(Choice::intentName).toBe(intentName)
+fun Expect<Choice>.toHaveIntent(intentName: String) {
+    feature(Choice::intentName).toBe(intentName)
 }
 
-fun Assert<Choice>.toHaveParameter(
+fun Expect<Choice>.toHaveParameter(
     key: ParameterKey,
     value: String,
 ) {
-    property(Choice::parameters).addAssertionsCreatedBy {
-        returnValueOf(Map<String, String>::get, key.name).toBe(value)
+    feature(Choice::parameters) {
+        feature(Map<String, String>::get, key.name).toBe(value)
     }
 }
 
-fun Assert<Choice>.toHaveParameter(
+fun Expect<Choice>.toHaveParameter(
     key: String,
     value: String,
 ) {
-    property(Choice::parameters).addAssertionsCreatedBy {
-        returnValueOf(Map<String, String>::get, key).toBe(value)
+    feature(Choice::parameters) {
+        feature(Map<String, String>::get, key).toBe(value)
     }
 }
