d10be5138697a12333a416e38a0c21bfeb34224c
Mohamed ASSOUKTI <mohamed.assoukti@partnre.com>
2026-02-13 19:22:51 +0100

    LLM Reasoning config

--- gen-ai/orchestrator-server/src/main/python/server/src/gen_ai_orchestrator/configurations/environment/settings.py
+++ gen-ai/orchestrator-server/src/main/python/server/src/gen_ai_orchestrator/configurations/environment/settings.py
@@ -56,21 +56,24 @@ class _Settings(BaseSettings):
 
     application_environment: _Environment = _Environment.DEV
     application_logging_config_ini: str = (
-            Path(__file__).dirname() + '/../logging/config.ini'
+        Path(__file__).dirname() + '/../logging/config.ini'
     )
     """Request timeout: set the maximum time (in seconds) for the request to be completed."""
     llm_provider_timeout: int = 30
     llm_provider_max_retries: int = 0
     """ Enable or not the rate limit for the LLM call"""
     llm_rate_limits: bool = True
+    llm_reasoning_effort: str = 'minimal'
     em_provider_timeout: int = 4
 
-    vector_store_provider: Optional[VectorStoreProvider] = VectorStoreProvider.OPEN_SEARCH
+    vector_store_provider: Optional[
+        VectorStoreProvider
+    ] = VectorStoreProvider.OPEN_SEARCH
     vector_store_host: Optional[str] = 'localhost'
     vector_store_port: Optional[str] = '9200'
     vector_store_user: Optional[str] = 'admin'
     vector_store_pwd: Optional[str] = 'admin'
-    vector_store_database: Optional[str] = None # Only if necessary. Example: PGVector
+    vector_store_database: Optional[str] = None  # Only if necessary. Example: PGVector
     vector_store_secret_manager_provider: Optional[SecretManagerProvider] = None
     vector_store_credentials_secret_name: Optional[str] = None
     """Request timeout: set the maximum time (in seconds) for the request to be completed."""
